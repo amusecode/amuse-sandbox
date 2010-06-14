7d45282ed67d74577acab168efdc67c3f1636324
F.I. Pelupessy <pelupes@strw.leidenuniv.nl>
2010-06-14 11:37:20 +0000

    capreole gravity test
    git-svn-id: http://www.amusecode.org/svn/trunk/sandbox@1127 6e832d89-e7d1-4ad1-8ca1-873beecccf65

---
+++ pelupes/capreole_grav.py
@@ -0,0 +1,102 @@
+import os
+import sys
+
+from amuse.legacy.capreole.interface import Capreole
+from amuse.legacy.capreole.interface import GLCapreole
+
+import numpy
+
+from matplotlib import pyplot
+
+gamma=5./3
+ggrav=980.
+
+def constantdens_constantgrav(N,L):
+  instance=GLCapreole()
+  instance.setup_module()
+  instance.setup_mesh(N/10,N/10,N,L/10.,L/10.,L)
+  x,y,z=numpy.indices( (N/10,N/10,N) )
+  x=x.flatten()+1
+  y=y.flatten()+1
+  z=z.flatten()+1
+  rho=0.001*numpy.ones_like(x)
+  rhvx=0.*numpy.ones_like(x)
+  rhvy=0.*numpy.ones_like(x)
+  rhvz=0.*numpy.ones_like(x)
+  en=1.e6*numpy.ones_like(x)
+  instance.fill_grid_state(x,y,z,rho,rhvx,rhvy,rhvz,en)
+  
+  gx=0.*numpy.ones_like(x)
+  gy=0.*numpy.ones_like(x)
+  gz=-980.*numpy.ones_like(x)
+  err=instance.set_gravity_field(x,y,z,gx,gy,gz)
+
+  instance.initialize_grid(0.0)
+  instance.viewer()
+  return instance
+
+def eqdens_constantgrav(N,L):
+  instance=GLCapreole(debugger='xterm')
+  instance.setup_module()
+  instance.setup_mesh(N/10,N/10,N,L/10.,L/10.,L)
+  ix,iy,iz=numpy.indices( (N/10,N/10,N) )
+  ix=ix.flatten()+1
+  iy=iy.flatten()+1
+  iz=iz.flatten()+1
+  x,y,z,err=instance.get_position_of_index(ix,iy,iz)
+  rho0=0.001
+  pres0=1.e6
+  c2=gamma*pres0/rho0
+  zscl=c2/ggrav/gamma
+  rho=rho0*numpy.exp(-z/zscl)
+  rhvx=0.*numpy.ones_like(rho)
+  rhvy=0.*numpy.ones_like(rho)
+  rhvz=0.*numpy.ones_like(rho)
+  en=pres0*numpy.exp(-z/zscl)/(gamma-1.)
+  
+  instance.fill_grid_state(ix,iy,iz,rho,rhvx,rhvy,rhvz,en)
+
+  ix,iy,iz=numpy.indices( (N/10,N/10,N) )
+  ix=ix.flatten()+1
+  iy=iy.flatten()+1
+  iz=iz.flatten()+1
+  gx=0.*numpy.ones_like(ix)
+  gy=0.*numpy.ones_like(ix)
+  gz=-ggrav*numpy.ones_like(ix)
+  err=instance.set_gravity_field(ix,iy,iz,gx,gy,gz) 
+  rs="ref_shift"
+  rf="reflective"
+  err=instance.set_boundary(rf,rf,rf,rf,rf,rf)
+  print err
+  instance.initialize_grid(0.0)
+  if hasattr(instance,'viewer'):
+    instance.viewer()
+  return instance
+
+
+def plot(x,rho,pres,vel,U):
+  pyplot.subplot( 221)
+  pyplot.plot(x,rho)
+  pyplot.subplot( 222)
+  pyplot.plot(x,pres)
+  pyplot.subplot( 223)
+  pyplot.plot(x,vel)
+  pyplot.subplot( 224)
+  pyplot.plot(x,u)
+  pyplot.savefig("plot.png")
+
+if __name__=="__main__":
+  N=100
+  instance=eqdens_constantgrav(N,1.e7)
+  instance.evolve(1000.)
+  ix,iy,iz=numpy.indices( (1,1,100) )
+  ix=ix.flatten()+N/20  
+  iy=iy.flatten()+N/20  
+  iz=iz.flatten()+1
+  x,y,z,err=instance.get_position_of_index(ix,iy,iz)
+  rho,rhovx,rhovy,rhovz,en,err=instance.get_grid_state(ix,iy,iz)
+  vel=rhovz/rho
+  pres=(gamma-1)*(en-0.5*rho*vel**2)
+  u=pres/(gamma-1)/rho
+  plot(z/1.e5,rho,pres,vel,u)
+  instance.stop()
