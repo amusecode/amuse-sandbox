0d6d262dabc0adca7d6bcbbd415996449eea6904
Marcell Marosvolgyi <marosvolgyi@strw.leidenuniv.nl>
2011-03-03 14:06:17 +0000

    #189 get_potential gadget2
    git-svn-id: http://www.amusecode.org/svn/trunk/sandbox@1893 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- marosvolgyi/amuselib/example.py
+++ marosvolgyi/amuselib/example.py
@@ -8,11 +8,11 @@ import numpy as np
 if __name__ == '__main__':
 
     instance = supportInterface(redirection='none')
-    x = np.zeros(1000)
-    y = np.zeros(1000)
-    z = np.zeros(1000)
-    #R = instance.many_points_on_sphere(x,y,z)
-    R = instance.rnd_points_on_sphere(x,y,z)
+    x = np.zeros(2000)
+    y = np.zeros(2000)
+    z = np.zeros(2000)
+    R = instance.many_points_on_sphere(x,y,z)
+    #R = instance.rnd_points_on_sphere(x,y,z)
     x1 = R.x
     y1 = R.y
     z1 = R.z
