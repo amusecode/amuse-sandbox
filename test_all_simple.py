71f7acc5cfc6f2ecca5e2f8a9836e9a4dd209d34
Nathan de Vriesn <devries.nathan@gmail.com>
2011-03-30 08:30:02 +0000

    Fixed the simple examples test
    git-svn-id: http://www.amusecode.org/svn/trunk/sandbox@2013 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- test_all_simple.py
+++ test_all_simple.py
@@ -4,7 +4,7 @@ from amuse.community import get_amuse_root_dir
 from amuse.test.amusetest import TestCase
 
 amuse_sh_path = path.join(get_amuse_root_dir(), 'amuse.sh')
-simple_examples_dir = path.dirname(path.abspath(__file__))
+simple_examples_dir = path.join(get_amuse_root_dir(), 'examples', 'simple')
 simple_scripts = [one_file for one_file in listdir(simple_examples_dir) if one_file[-3:] == '.py']
 
 def path_to_script(filename):
