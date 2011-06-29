9706abbe3f7fc21381350b6b0a8e282d7730f345
F.I. Pelupessy <pelupes@strw.leidenuniv.nl>
2011-06-29 14:22:22 +0000

    alternative kepler solver
    git-svn-id: http://www.amusecode.org/svn/trunk/sandbox@2261 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- janes/huayno2/evolve_twobody.c
+++ janes/huayno2/evolve_twobody.c
@@ -1,12 +1,12 @@
-#include "twobody/kepler.c"
+#include "twobody/universal_variable_kepler.c"
 
 void evolve_twobody(struct sys s, DOUBLE stime, DOUBLE etime, DOUBLE dt) {
   clevel++;
   if (etime <= stime ||  dt==0 || clevel>=MAXLEVEL) ENDRUN("timestep too small\n");
   if (s.n != 2) ENDRUN("two-body solver was called with sys.n=%u\n", s.n);
   // translate coordinates original frame to 2-body frame
-  DOUBLE dpos[3];
-  DOUBLE dvel[3];
+  DOUBLE dpos[3],dpos0[3];
+  DOUBLE dvel[3],dvel0[3];
   DOUBLE pos_cm[3];
   DOUBLE vel_cm[3];
   DOUBLE m1 = s.part->mass;
@@ -14,12 +14,12 @@ void evolve_twobody(struct sys s, DOUBLE stime, DOUBLE etime, DOUBLE dt) {
   DOUBLE mtot = s.part->mass + s.last->mass;
   DOUBLE f1 = m1 / mtot;
   DOUBLE f2 = m2 / mtot;
-  dpos[0] = s.part->pos[0] - s.last->pos[0];
-  dpos[1] = s.part->pos[1] - s.last->pos[1];
-  dpos[2] = s.part->pos[2] - s.last->pos[2];
-  dvel[0] = s.part->vel[0] - s.last->vel[0];
-  dvel[1] = s.part->vel[1] - s.last->vel[1];
-  dvel[2] = s.part->vel[2] - s.last->vel[2];
+  dpos0[0] = s.part->pos[0] - s.last->pos[0];
+  dpos0[1] = s.part->pos[1] - s.last->pos[1];
+  dpos0[2] = s.part->pos[2] - s.last->pos[2];
+  dvel0[0] = s.part->vel[0] - s.last->vel[0];
+  dvel0[1] = s.part->vel[1] - s.last->vel[1];
+  dvel0[2] = s.part->vel[2] - s.last->vel[2];
   pos_cm[0] = (m1 * s.part->pos[0] + m2 * s.last->pos[0]) / mtot;
   pos_cm[1] = (m1 * s.part->pos[1] + m2 * s.last->pos[1]) / mtot;
   pos_cm[2] = (m1 * s.part->pos[2] + m2 * s.last->pos[2]) / mtot;
@@ -27,12 +27,10 @@ void evolve_twobody(struct sys s, DOUBLE stime, DOUBLE etime, DOUBLE dt) {
   vel_cm[1] = (m1 * s.part->vel[1] + m2 * s.last->vel[1]) / mtot;
   vel_cm[2] = (m1 * s.part->vel[2] + m2 * s.last->vel[2]) / mtot;
   // call kepler solver
-  kepler_set_position(dpos);
-  kepler_set_velocity(dvel);
-  set_mu(mtot);
-  if (kepler_evolve(dt) != 0) ENDRUN("kepler_evolve() failed\n");
-  kepler_get_position(dpos);
-  kepler_get_velocity(dvel);
+
+  {int err=universal_variable_kepler_solver(dt,mtot,dpos0,dvel0,dpos,dvel);
+  if ( err != 0) ENDRUN("kepler_evolve() failed %d\n",err);
+  }
   // evolve center of mass for dt
   pos_cm[0] += vel_cm[0] * dt;
   pos_cm[1] += vel_cm[1] * dt;
