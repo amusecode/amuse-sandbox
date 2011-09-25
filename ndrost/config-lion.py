a40560c83621ea49138ec61bb7a31be7ac977954
Niels Drost <n.drost@esciencecenter.nl>
2011-09-25 14:28:48 +0000

    (mostly) working configuration for OSX Lion
    git-svn-id: http://www.amusecode.org/svn/trunk/sandbox@2548 6e832d89-e7d1-4ad1-8ca1-873beecccf65

---
+++ ndrost/config-lion.py
@@ -0,0 +1,43 @@
+#
+# support/config.py.  Generated from configpy.in by configure.
+#
+
+class interpreters(object):
+    python = '/opt/local/bin/python2.6' 
+
+class compilers(object):
+    cxx = 'g++-mp-4.4' 
+    cc  = 'gcc-mp-4.4'
+    fc = 'gfortran-mp-4.4'
+    
+    cxx_flags = '-m64'
+    cc_flags  = '-m64'
+    fc_flags = '-m64'
+    
+    found_fftw = 'yes'
+    fftw_flags = '-I /opt/local/include'
+    fftw_libs = '-L /opt/local/lib/'
+    
+    found_gsl = 'yes'
+    gsl_flags = '-I /opt/local/include'
+    gsl_libs = '-L /opt/local/lib/'
+    
+
+class mpi(object):
+    mpicxx = 'openmpicxx' 
+    mpicc  = 'openmpicc'
+    mpif95 = 'openmpif90'
+
+class java(object):
+    jni_includes = '-I/System/Library/Frameworks/JavaVM.framework/Headers'
+    jdk = '/System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home'
+
+class cuda(object):
+    is_enabled   = 'yes'=='yes'
+    compiler     = '/usr/local/cuda/bin/nvcc'
+    toolkit_path = '/usr/local/cuda'
+    sdk_path     = '/Developer/CUDA/C'
+    
+class modules(object):
+    have_matplotlib = 1==1
+    have_h5py       = 1==1
