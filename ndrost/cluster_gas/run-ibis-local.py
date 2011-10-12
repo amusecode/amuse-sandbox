ba2c3edeb1185272ca17fcded4505e37d0f459b4
Niels Drost <n.drost@esciencecenter.nl>
2011-10-12 14:53:37 +0000

    run on localhost with ibis
    git-svn-id: http://www.amusecode.org/svn/trunk/sandbox@2607 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- ndrost/cluster_gas/run-ibis-local.py
+++ ndrost/cluster_gas/run-ibis-local.py
@@ -13,31 +13,27 @@ from amuse.community.bhtree.interface import BHTree
 from amuse.community.phiGRAPE.interface import PhiGRAPE
 from SSEplus import SSEplus
 
-#nice big star with 100 stars
-#numpy.random.seed(123489)
 
-#best guess inti's original seed
-numpy.random.seed(123491)
+numpy.random.seed(123489)
 
 clustergas.clustergas(sfeff=0.3, 
-                    Nstar=1000,
-                    Ngas=100000,
+                    Nstar=100,
+                    Ngas=10000,
                     Rscale=0.5 | units.parsec,
-                    feedback_efficiency=0.1,
+                    feedback_efficiency=0.01,
                     runid="demo-output",
-		    dt_plot=0.25 | units.Myr,
 
                     grav_code=PhiGRAPE,
-                    grav_code_extra=dict(mode='gpu', channel_type='sockets', hostname='localhost'),
+                    grav_code_extra=dict(mode='normal', channel_type='ibis', hostname='localhost'),
 
                     gas_code=Gadget2,
-                    gas_code_extra=dict(number_of_workers=64,number_of_nodes=8,use_gl=False, channel_type='ibis', hostname='VU'),
+                    gas_code_extra=dict(number_of_workers=4,number_of_nodes=1,use_gl=False, channel_type='ibis', hostname='localhost'),
 
                     se_code=SSEplus,
-                    se_code_extra=dict(channel_type='sockets', hostname='localhost'),
+                    se_code_extra=dict(channel_type='ibis', hostname='localhost'),
 
-                    grav_couple_code=Octgrav,
-                    grav_couple_code_extra=dict(channel_type='sockets', hostname='localhost')
+                    grav_couple_code=Fi,
+                    grav_couple_code_extra=dict(channel_type='ibis', hostname='localhost')
 )
 
 
