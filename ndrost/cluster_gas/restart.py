3ef5bbf2dacdf352bc99c41577f6c8ae57f82b4f
Niels Drost <n.drost@esciencecenter.nl>
2011-11-02 10:11:21 +0000

    improved sge script
    git-svn-id: http://www.amusecode.org/svn/trunk/sandbox@2731 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- ndrost/cluster_gas/restart.py
+++ ndrost/cluster_gas/restart.py
@@ -6,10 +6,10 @@ from amuse.support.units import units
 
 clustergas.clustergas_restart(
                    "demo-output",
-                   481,
+                   1136,
 		   dt_plot=0.01 | units.Myr,
                    grav_code_extra=dict(mode='gpu', channel_type='sockets', hostname='localhost'),
-                   gas_code_extra=dict(number_of_workers=8,number_of_nodes=1,use_gl=False, channel_type='ibis', hostname='local'),
+                   gas_code_extra=dict(number_of_workers=8,number_of_nodes=1,use_gl=False, channel_type='ibis', hostname='VU'),
                    se_code_extra=dict(channel_type='sockets', hostname='localhost'),
                    grav_couple_code_extra=dict(channel_type='sockets', hostname='localhost')
 
