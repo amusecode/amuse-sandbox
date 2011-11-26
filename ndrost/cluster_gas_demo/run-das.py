cfb0c582235a012736c310bf424f600c961d89ef
Niels Drost <n.drost@esciencecenter.nl>
2011-11-26 08:52:23 +0000

    demo version of cluster_gas simulation
    git-svn-id: http://www.amusecode.org/svn/trunk/sandbox@2856 6e832d89-e7d1-4ad1-8ca1-873beecccf65

---
+++ ndrost/cluster_gas_demo/run-das.py
@@ -0,0 +1,49 @@
+"""
+ run script for clustergas runs
+
+"""
+
+import numpy
+import clustergas_gadget as clustergas
+from amuse.units import units
+from amuse.community.fi.interface import Fi
+from amuse.community.octgrav.interface import Octgrav
+from amuse.community.gadget2.interface import Gadget2
+from amuse.community.bhtree.interface import BHTree
+from amuse.community.phiGRAPE.interface import PhiGRAPE
+from SSEplus import SSEplus
+
+#nice big star with 100 stars
+#numpy.random.seed(123489)
+
+#best guess inti's original seed
+numpy.random.seed(123491)
+
+#                    gas_code_extra=dict(number_of_workers=8,number_of_nodes=1,use_gl=False, channel_type='ibis', hostname='VU'),
+
+clustergas.clustergas(sfeff=0.3, 
+                    Nstar=1000,
+                    Ngas=100000,
+                    Rscale=0.5 | units.parsec,
+#                    feedback_efficiency=0.1,
+                    runid="demo-output",
+#		    dt_plot=0.25 | units.Myr,
+
+                    grav_code=PhiGRAPE,
+                    grav_code_extra=dict(mode='gpu', channel_type='ibis', hostname='VU-GPU'),
+
+                    gas_code=Gadget2,
+                    gas_code_extra=dict(number_of_workers=1,use_gl=False, channel_type='ibis'),
+
+                    se_code=SSEplus,
+                    se_code_extra=dict(channel_type='ibis', hostname='VU'),
+
+                    grav_couple_code=Fi,
+                    grav_couple_code_extra=dict(channel_type='ibis', hostname='VU')
+)
+
+
+#clustergas.clustergas_restart(
+#                   "test",
+#                   250,newid='test_restart')
+
