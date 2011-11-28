3062ad8b0c25964000c4b159fe2f459b49ddc3f9
F.I. Pelupessy <pelupes@strw.leidenuniv.nl>
2011-11-28 16:15:00 +0000

    update of test script
    (to be fixed)
    
    
    git-svn-id: http://www.amusecode.org/svn/trunk/sandbox@2860 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- pelupes/simplex2_5/simplex2_5_test.py
+++ pelupes/simplex2_5/simplex2_5_test.py
@@ -18,6 +18,7 @@ from amuse.support.io import write_set_to_file
 write_snapshots=True
 
 
+gamma=5./3.
 mu=1.| units.amu
 muion =0.5 | units.amu
 xtrans=0.06
@@ -26,9 +27,9 @@ Tinit=100. | units.K
 Ttrans=13500. | units.K 
 Tion=13500. | units.K
 rhoinit=0.001 | (units.amu / units.cm**3)
-uinit=1.5*constants.kB * Tinit/mu
-utrans=1.5*constants.kB * Ttrans/mutrans
-uion=1.5*constants.kB * Tion/muion
+uinit=1/(gamma-1)*constants.kB * Tinit/mu
+utrans=1/(gamma-1)*constants.kB * Ttrans/mutrans
+uion=1/(gamma-1)*constants.kB * Tion/muion
 mp=None
 
 def fake_internal_energy_from_xion(xion):
@@ -291,7 +292,7 @@ def radhydro_evolve_fake(sph,rad,tend,dt):
 def rad_only_evolve(sph,rad,tend,dt):
   global label
 
-  i=0
+  i=    i=t.value_in(units.Myr)
   p=sph.particles.copy()
   channel = rad.particles.new_channel_to(p)
   channel.copy_attributes(["xion","rho","u"])  
@@ -307,7 +308,7 @@ def rad_only_evolve(sph,rad,tend,dt):
     rad.evolve_model(t+dt)
     
     t+=dt
-    i+=1
+    i=t.value_in(units.Myr)
 
     if i%10==0: 
       p=sph.particles.copy()
@@ -315,7 +316,7 @@ def rad_only_evolve(sph,rad,tend,dt):
       channel.copy_attributes(["xion","rho","u"])  
       print (2./3.*mu/constants.kB*p.u.amin()).in_(units.K), (2./3.*mu/constants.kB*p.u.amax()).in_(units.K)
       if write_snapshots:
-        write_set_to_file(p,label+"-%6.6i"%i,"amuse",
+        write_set_to_file(p,label+"-%6.6i"%int(i),"amuse",
                           append_to_file=False)    
 
 
@@ -347,12 +348,11 @@ def aplot(i, tag, xyc,xlim=None,ylim=None,ylabel=""):
   pyplot.savefig(tag+'-%6.6i.png'%i)
 
 
-def test(evolver, rad_parameters=None):    
-  N=10000
+def test(evolver, rad_parameters=None,dt=1. | units.Myr ):    
+  N=100000
   Ns=10
   L=15. | units.kpc
-  dt=1. | units.Myr  
-  tend=200.| units.Myr
+  tplot=[10.| units.Myr,200.| units.Myr,500.| units.Myr]
 
   if rad_parameters==None:
     rad_parameters=dict(box_size=2*L)
@@ -361,44 +361,48 @@ def test(evolver, rad_parameters=None):
 
   sph,rad=iliev_test_5(N,Ns,L,dt/2.,rad_parameters=rad_parameters)
 
-  evolver(sph,rad,tend,dt)
+  for tend in tplot:
+    evolver(sph,rad,tend,dt)
 
-  p=rad.particles.copy()
-  channel = sph.particles.new_channel_to(p)
-  if evolver==rad_only_evolve:
-    channel.copy_attributes(["vx","vy","vz"])  
-  else:
-    channel.copy_attributes(["vx","vy","vz","u"])  
+    p=rad.particles.copy()
+    channel = sph.particles.new_channel_to(p)
+    if evolver==rad_only_evolve:
+      channel.copy_attributes(["vx","vy","vz"])  
+    else:
+      channel.copy_attributes(["vx","vy","vz","u"])  
 
-  r=(((p.x)**2+(p.y)**2+(p.z)**2) ** 0.5).value_in(units.kpc)
-  v=((p.vx**2+p.vy**2+p.vz**2)**0.5).value_in(units.kms)
-  cs=(p.u**0.5).value_in(units.kms)
-  xion=p.xion.number
-  t=2./3.*mu/(1.+xion)/constants.kB*p.u
-  t=t.value_in(units.K)
-  dens=(p.rho).value_in(units.amu/units.cm**3)
-  pres=(p.u*p.rho).value_in( 100*units.g/units.cm/units.s**2)
-  mach=v/cs+1.e-10
+    r=(((p.x)**2+(p.y)**2+(p.z)**2) ** 0.5).value_in(units.kpc)
+    v=((p.vx**2+p.vy**2+p.vz**2)**0.5).value_in(units.kms)
+    cs=(gamma*(gamma-1)*p.u**0.5).value_in(units.kms)
+    xion=p.xion.number
+    t=(gamma-1)*mu/(1.+xion)/constants.kB*p.u
+    t=t.value_in(units.K)
+    dens=(p.rho).value_in(units.amu/units.cm**3)
+    pres=(p.u*p.rho).value_in( units.g/units.cm/units.s**2)
+    mach=v/cs+1.e-10
 
-  pyplot.figure( figsize=(8,6) ) 
-  pyplot.plot( r,xion,'r.')
-  pyplot.savefig('rx.png')
+    i=int(tend.value_in(units.Myr))
 
-  pyplot.figure( figsize=(8,6) ) 
-  pyplot.plot( r,t,'r.')
-  pyplot.savefig('rt.png')
+    pyplot.figure( figsize=(8,6) ) 
+    pyplot.plot( r,xion,'r.')
+    pyplot.savefig('rx-%6.6i.png'%i)
 
-  i=int(tend.value_in(units.Myr))
-  aplot(i,label+'-xion',((r/15,xion,'r'),(r/15,1-xion,'g')),
-          xlim=(0.,1.),ylim=(1.e-6,1.),ylabel='x,(1-x)')
-  aplot(i,label+'-temp',((r/15,t,'r'),),
-          xlim=(0.,1.),ylim=(100,1.e5),ylabel="T (K)")
-  aplot(i,label+'-pres',((r/15,pres,'r'),),
-          xlim=(0.,1.),ylim=(1.e-17,1.e-14),ylabel="pres (g/cm/s**2)")
-  aplot(i,label+'-rho',((r/15,dens,'r'),),
-          xlim=(0.,1.),ylim=(0.0001,0.01),ylabel='density (amu/cm**3)')
-  aplot(i,label+'-mach',((r/15,mach,'r'),),
-          xlim=(0.,1.),ylim=(1.e-5,10.),ylabel='Mach')
+    pyplot.figure( figsize=(8,6) ) 
+    pyplot.plot( r,t,'r.')
+    pyplot.savefig('rt-%6.6i.png'%i)
+
+    aplot(i,label+'-xion',((r/15,xion,'r'),(r/15,1-xion,'g')),
+            xlim=(0.,1.),ylim=(1.e-6,1.),ylabel='x,(1-x)')
+    aplot(i,label+'-temp',((r/15,t,'r'),),
+            xlim=(0.,1.),ylim=(100,1.e5),ylabel="T (K)")
+    aplot(i,label+'-pres',((r/15,pres,'r'),),
+            xlim=(0.,1.),ylim=(1.e-17,1.e-14),ylabel="pres (g/cm/s**2)")
+    aplot(i,label+'-rho',((r/15,dens,'r'),),
+            xlim=(0.,1.),ylim=(0.0001,0.01),ylabel='density (amu/cm**3)')
+    aplot(i,label+'-mach',((r/15,mach,'r'),),
+            xlim=(0.,1.),ylim=(1.e-5,10.),ylabel='Mach')
+    aplot(i,label+'-vel',((r/15,v,'r'),),
+            xlim=(0.,1.),ylim=(0.1,15.),ylabel='v (km/s)')
 
 
 
@@ -406,7 +410,7 @@ if __name__=="__main__":
   global label
   import time
   
-  
+  """  
   t1=time.time()
   label="test1"
   test(rad_only_evolve)
@@ -427,7 +431,7 @@ if __name__=="__main__":
   t2=time.time()
   print "test3: 5 freq, non dynamic, with thermal solver",
   print t2-t1,"sec"
-  
+
   t1=time.time()
   label="test4"
   test(radhydro_evolve_fake)
@@ -449,4 +453,17 @@ if __name__=="__main__":
   t2=time.time()
   print "test6: 5 freq, dynamic, thermal solver",
   print t2-t1,"sec"
-
+  """
+  t1=time.time()
+  label="test7"
+  test(radhydro_evolve, dict(number_of_freq_bins=10, blackbody_spectrum_flag=1,thermal_evolution_flag=1) )
+  t2=time.time()
+  print "test7: 10 freq, dynamic, thermal solver",
+  print t2-t1,"sec"
+  
+  t1=time.time()
+  label="test8"
+  test(radhydro_evolve, dict(number_of_freq_bins=5, blackbody_spectrum_flag=1,thermal_evolution_flag=1),dt=0.5|units.Myr )
+  t2=time.time()
+  print "test8: 5 freq, dynamic, thermal solver,dt=0.5 Myr",
+  print t2-t1,"sec"
