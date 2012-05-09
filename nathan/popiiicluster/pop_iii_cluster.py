e149232a703aa3433f577518d503f6f5f7a0e6da
Nathan de Vriesn <devries.nathan@gmail.com>
2012-05-09 09:37:57 +0000

    Update sandbox script
    git-svn-id: http://www.amusecode.org/svn/trunk/sandbox@3381 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- nathan/popiiicluster/pop_iii_cluster.py
+++ nathan/popiiicluster/pop_iii_cluster.py
@@ -1,82 +1,118 @@
-import numpy 
+import os
+import os.path
+import shutil
+import numpy
 
 from amuse.units import nbody_system, units
 from amuse.datamodel import Particle, ParticlesSuperset
 from amuse.support.exceptions import AmuseException
+from amuse.couple.parallel_stellar_evolution import ParallelStellarEvolution
+from amuse.io import write_set_to_file
 
 from amuse.community.ph4.interface import ph4
+from amuse.community.hermite0.interface import Hermite
 from amuse.community.mesa.interface import MESA
 from amuse.community.sse.interface import SSE
 from amuse.community.mmams.interface import MakeMeAMassiveStar
 
 from amuse.ic.plummer import new_plummer_model
+from amuse.ic.kingmodel import new_king_model
 from amuse.ic.salpeter import new_salpeter_mass_distribution
+from amuse.ic.brokenimf import new_scalo_mass_distribution
 from amuse.ic.flatimf import new_flat_mass_distribution
 
 
-def new_gravity_code(stars, convert_nbody):
-    gravity = ph4(convert_nbody, mode='gpu', redirection='file', redirect_file='gravity_code_out.log')
-    gravity.stopping_conditions.collision_detection.enable()
-    gravity.particles.add_particles(stars)
-    return gravity
+def new_working_directory():
+    i = 0
+    current_directory = os.getcwd()
+    while os.path.exists(os.path.join(current_directory, "run_{0:=03}".format(i))):
+        i += 1
+    new_directory = os.path.join(current_directory, "run_{0:=03}".format(i))
+    os.mkdir(new_directory)
+    print "Created new directory for output:", new_directory
+    os.mkdir(os.path.join(new_directory, "logs"))
+    shutil.copy(__file__, new_directory)
+    os.chdir(new_directory)
 
-def new_collision_code():
-    star_collider = MakeMeAMassiveStar(redirection='file', redirect_file='star_collider_code_out.log')
-    star_collider.initialize_code()
-    star_collider.parameters.dump_mixed_flag = True
-    star_collider.parameters.target_n_shells_mixing = 2000
-    star_collider.commit_parameters()
-    return star_collider
+def new_gravity_code(stars, convert_nbody):
+    gravity = ph4(convert_nbody, mode='gpu', channel_type='ibis', hostname='lgm02', 
+        redirection='file', redirect_file=os.path.join('logs', 'gravity_code_out.log'))
+    gravity.particles.add_particles(stars)
+    gravity.stopping_conditions.collision_detection.enable()
+    return gravity
 
 def new_stellar_evolution_codes(stars):
     n_low_mass = (stars.mass < 10|units.MSun).sum()
     print n_low_mass, "particles in SSE,", len(stars)-n_low_mass, "particles in MESA"
     
-    stellar_evolution = MESA(redirection='file', redirect_file='stellar_evolution_code_out.log')
-    stellar_evolution.parameters.RGB_wind_scheme = 4 # 'Dutch' wind
-    stellar_evolution.parameters.AGB_wind_scheme = 4 # 'Dutch' wind
-#    stellar_evolution.parameters.dutch_wind_efficiency = 0.8
+#~    number_of_workers = 54
+    number_of_workers = 4
+    nodes = ['lgm00', 'lgm02', 'lgm03', 'lgm04', 'lgm05', 'lgm06', 'lgm07', 'lgm08', 'lgm09', 
+        'lgm10', 'lgm11', 'lgm12', 'lgm13', 'lgm14', 'lgm15', 'lgm16', 'lgm17', 'lgm19']
+    
+    n_nodes = len(nodes)
+    individual_options = [dict(
+        hostname=nodes[i%n_nodes], 
+        redirect_file=os.path.join("logs", "stellar_evolution_code_out_{0}.log".format(i))
+    ) for i in range(number_of_workers)]
+    
+    stellar_evolution = ParallelStellarEvolution(MESA, number_of_workers=number_of_workers, 
+        channel_type='ibis', redirection='file', individual_options=individual_options,
+        output_data_root_directory="/home/vriesn/amuse-svn/data", 
+        input_data_root_directory="/home/vriesn/amuse-svn/data", 
+        default_path_to_MESA_data="/home/vriesn/amuse-svn/src/amuse/community/mesa/src/mesa/data")
+    
     stellar_evolution.parameters.metallicity = 0.0
     stellar_evolution.particles.add_particles(stars[n_low_mass:])
     
-    se_light = SSE(redirection='file', redirect_file='low_mass_stellar_evolution_code_out.log')
+    se_light = SSE(redirection='file', redirect_file=os.path.join("logs", "low_mass_stellar_evolution_code_out.log"))
     se_light.parameters.metallicity = 0.0001
     se_light.particles.add_particles(stars[:n_low_mass])
     return stellar_evolution, se_light
 
+def move_from_light_to_real_stellar_evolution(missing, se_light, stellar_evolution):
+    print "   Move the missing particles from the 'light' code to the 'real' stellar evolution code:\n", missing
+    vivified = stellar_evolution.particles.add_particles(missing)
+    for star in vivified:
+        star.evolve_for(stellar_evolution.model_time)
+    se_light.particles.remove_particles(missing)
+    print vivified
+
 def main():
-    number_of_stars = 1000
+    number_of_stars = 35000
     time_end = 10.0 | units.Myr
-    delta_t = 0.01 | units.Myr
+    delta_t = 0.0001 | units.Myr
+    initial_mass_function = new_scalo_mass_distribution # or new_salpeter_mass_distribution or new_flat_mass_distribution
 
     numpy.random.seed(12345)
-#    masses = new_salpeter_mass_distribution(number_of_stars, 
-    masses = new_flat_mass_distribution(number_of_stars, 
-        mass_min = 0.1 | units.MSun, mass_max = 100 | units.MSun).sorted()
+    masses = initial_mass_function(number_of_stars, mass_max = 100 | units.MSun).sorted()
+    print "Drew {0} random masses from IMF using {1}.\n  maximum={2},\n  mean={3},\n  median={4}".format(
+        number_of_stars, initial_mass_function.__name__, masses.amax(), masses.mean(), masses.median())
     
-    convert_nbody = nbody_system.nbody_to_si(masses.sum(), 0.01 | units.parsec)
-    stars = new_plummer_model(number_of_stars, convert_nbody)
+    convert_nbody = nbody_system.nbody_to_si(masses.sum(), 0.31 | units.parsec)
+    stars = new_king_model(number_of_stars, 6, convert_nbody)
     stars.mass = masses
     stars.scale_to_standard(convert_nbody=convert_nbody)
 
     gravity = new_gravity_code(stars, convert_nbody)
-    star_collider = new_collision_code()
+    collision_detection = gravity.stopping_conditions.collision_detection
     stellar_evolution, se_light = new_stellar_evolution_codes(stars)
     
     all_se_particles = ParticlesSuperset([se_light.particles, stellar_evolution.particles])
     from_stellar_evolution_to_gravity = all_se_particles.new_channel_to(gravity.particles)
     from_stellar_evolution_to_gravity.copy_attributes(["mass", "radius"])
 
-    print 'ICs:'
+    print 'Initial conditions:'
     print gravity.particles
     
     mergers = []
+    se_merge_products = stellar_evolution.particles[:0]
     
     while gravity.model_time < time_end:
         target_time = min(gravity.model_time + delta_t, time_end)
         gravity.evolve_model(target_time)
         gravity.synchronize_model()
-        print "\nCollision detected?", gravity.stopping_conditions.collision_detection.is_set()
+        print "\n   Gravity done, {0} collision(s) detected".format(len(collision_detection.particles(0)))
         try:
             stellar_evolution.evolve_model(gravity.model_time)
         except AmuseException:
@@ -84,108 +120,53 @@ def main():
             print mergers
             raise
         se_light.evolve_model(gravity.model_time)
+        print "   Stellar evolution done"
         from_stellar_evolution_to_gravity.copy_attributes(["mass", "radius"])
         
-        if gravity.stopping_conditions.collision_detection.is_set():
-            print '(' + str(len(gravity.stopping_conditions.collision_detection.particles(0))), 'collision(s) detected)'
-            gd_primaries = gravity.stopping_conditions.collision_detection.particles(0)
-            gd_secondaries = gravity.stopping_conditions.collision_detection.particles(1)
-            print gd_primaries + gd_secondaries
-            col_primaries = star_collider.particles.add_particles(gd_primaries)
-            col_secondaries = star_collider.particles.add_particles(gd_secondaries)
-            print "   Distance(s) between colliders:", (gd_primaries.position - gd_secondaries.position).lengths()
-            print "   Sum(s) of radii of colliders:", gd_primaries.radius + gd_secondaries.radius
+        if collision_detection.is_set():
+            gd_primaries = collision_detection.particles(0)
+            gd_secondaries = collision_detection.particles(1)
+            gd_colliders = gd_primaries + gd_secondaries
+            print "   Colliding gravity particles:"
+            print gd_colliders
             
-            se_colliders = star_collider.particles.get_intersecting_subset_in(stellar_evolution.particles)
-            if len(star_collider.particles) != len(se_colliders):
-                missing = star_collider.particles.get_intersecting_subset_in(se_light.particles)
-                print "   Move the missing particles from the 'light' code to the 'real' stellar evolution code:\n", missing
-                vivified = stellar_evolution.particles.add_particles(missing)
-                for star in vivified:
-                    star.evolve_for(stellar_evolution.model_time)
-                print vivified
-                se_light.particles.remove_particles(missing)
-                se_colliders = star_collider.particles.get_intersecting_subset_in(stellar_evolution.particles)
-                if len(star_collider.particles) != len(se_colliders):
-                    raise AmuseException("Number of colliding particles does not match the number of particles in", 
-                        star_collider.__class__.__name__, "!")
+            # Are there (low-mass) stars missing in the stellar evolution code (i.e. stored in the 'light' code)?
+            missing = gd_colliders.get_intersecting_subset_in(se_light.particles)
+            if len(missing) > 0:
+                move_from_light_to_real_stellar_evolution(missing, se_light, stellar_evolution)
             
-            for col_particle, se_particle in zip(star_collider.particles, se_colliders):
-                number_of_zones     = se_particle.get_number_of_zones()
-                mm1                 = se_particle.get_mass_profile(number_of_zones = number_of_zones)* se_particle.mass
-                mass_profile        = se_particle.get_cumulative_mass_profile(number_of_zones = number_of_zones) * se_particle.mass
-                density_profile     = se_particle.get_density_profile(number_of_zones = number_of_zones)
-                radius_profile      = se_particle.get_radius_profile(number_of_zones = number_of_zones)
-                temperature_profile = se_particle.get_temperature_profile(number_of_zones = number_of_zones)
-                lum                 = se_particle.get_luminosity_profile(number_of_zones = number_of_zones)
-                pressure_profile    = se_particle.get_pressure_profile(number_of_zones = number_of_zones)
-                mu_profile          = se_particle.get_mu_profile(number_of_zones = number_of_zones)
-                composition_profile = se_particle.get_chemical_abundance_profiles(number_of_zones = number_of_zones)
-                
-                col_particle.add_shell(mm1,mass_profile, radius_profile, density_profile, 
-                    pressure_profile, temperature_profile,lum, mu_profile, composition_profile[0], 
-                    composition_profile[1]+composition_profile[2], composition_profile[3], 
-                    composition_profile[4], composition_profile[5], composition_profile[6], 
-                    composition_profile[7], composition_profile[7]*0.0, composition_profile[7]*0.0)
+            se_merged, gd_merged = stellar_evolution.merge_colliding(gd_primaries, gd_secondaries, 
+                MakeMeAMassiveStar, dict(redirection='file', redirect_file=os.path.join('logs', 'star_collider_code_out.log')), 
+                dict(dump_mixed_flag = True, target_n_shells_mixing = 2000),
+                return_merge_products=["se", "gd"])
             
-            print "   Copying stellar structure of each particle to", star_collider.__class__.__name__, "done."
-            
-            for col_primary, col_secondary in zip(col_primaries, col_secondaries):
-                merge_product = Particle()
-                merge_product.primary = col_primary
-                merge_product.secondary = col_secondary
-                print "   Merging colliding stars in", star_collider.__class__.__name__
-                try:
-                    mmams_new_particle = star_collider.merge_products.add_particle(merge_product)
-                except AmuseException:
-                    print "NOTE: shock_heating_flag = 0"
-                    star_collider.set_do_shock_heating_flag(0)
-                    mmams_new_particle = star_collider.merge_products.add_particle(merge_product)
-                
-                stellar_model = mmams_new_particle.internal_structure()
-                merge_product.mass = stellar_model.mass[-1] 
-                merge_product.radius= stellar_model.radius[-1]
-                gd_colliders = (col_primary + col_secondary).get_intersecting_subset_in(gravity.particles)
-                merge_product.position = gd_colliders.center_of_mass()
-                merge_product.velocity = gd_colliders.center_of_mass_velocity()
-                print "   Merge product:\n", merge_product
-                mergers.append((gd_colliders.mass, merge_product.mass))
-                
-                stellar_evolution.new_particle_from_model(stellar_model, 0. | units.Myr, key=merge_product.key)
-                gravity.particles.add_particle(merge_product)
-                print "   Merge product has been added to gravity and stellar_evolution codes"
-
-            stellar_evolution.particles.remove_particles(star_collider.native_stars)
-            gravity.particles.remove_particles(star_collider.native_stars)
-            star_collider.particles.remove_particles(star_collider.particles)
-            print "   Original colliding particles have been removed from codes"
-            
-            print "   Stellar evolution particles:"
-            print stellar_evolution.particles
-            print "   Gravity particles:"
-            print gravity.particles
+            se_merge_products += se_merged
+            mergers.extend([[m1, m2, m] for m1, m2, m in zip(gd_primaries.mass.as_quantity_in(units.MSun), gd_secondaries.mass.as_quantity_in(units.MSun), gd_merged.mass)])
+            gravity.particles.remove_particles(gd_colliders)
+            gravity.particles.add_particles(gd_merged)
+            print "   Stellar evolution merge products so far:"
+            print se_merge_products
+            write_set_to_file(gravity.particles.copy_to_memory().savepoint(gravity.model_time), 'pop_iii_cluster_N{0}.hdf5'.format(number_of_stars), 'hdf5')
         
-        print "   Evolved system up to:", gravity.model_time.as_quantity_in(units.Myr)
-        print "   System currently contains", len(gravity.particles), "particles."
+        print "   Evolved system (currently {0} particles) up to: {1}".format(len(gravity.particles), gravity.model_time.as_quantity_in(units.Myr))
         max_radius = max(stellar_evolution.particles.radius).as_quantity_in(units.RSun)
         giant = stellar_evolution.particles.select(lambda x : x == max_radius, ["radius"])
         giant_in_gd = giant.get_intersecting_subset_in(gravity.particles)[0]
-        print "   Largest star has radius:", max_radius
-        print "   Nearest neighbour:", min(((gravity.particles-giant_in_gd).position - giant_in_gd.position).lengths_squared()).sqrt().as_quantity_in(units.RSun)
-        E_kin = gravity.particles.kinetic_energy()
-        E_pot = gravity.particles.potential_energy()
-        print "   Total energy:", E_kin + E_pot
-        print "   Energy ratio:", E_kin / E_pot
+        print "   Largest star has radius: {0}, and distance to nearest neighbour: {1}".format(max_radius, 
+            min(((gravity.particles-giant_in_gd).position - giant_in_gd.position).lengths_squared()).sqrt().as_quantity_in(units.RSun))
+        E_kin = gravity.kinetic_energy
+        E_pot = gravity.potential_energy
+        print "   Total energy K+V: {0}, ratio K/V: {1}".format(E_kin + E_pot, E_kin / E_pot)
+        
+#~        write_set_to_file(gravity.particles.copy_to_memory().savepoint(gravity.model_time), 'pop_iii_cluster_N{0}.hdf5'.format(number_of_stars), 'hdf5')
 
-    print 'Final model:'
-    print "   Stellar evolution particles:"
-    print stellar_evolution.particles
-    print "   Gravity particles:"
-    print gravity.particles
-    star_collider.stop()
     gravity.stop()
     stellar_evolution.stop()
     se_light.stop()
     
+    print "\nSimulation finished. Mergers so far:"
+    print mergers
+    
 if __name__ == '__main__':
+    new_working_directory()
     main()
