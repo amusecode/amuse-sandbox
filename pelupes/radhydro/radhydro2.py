4423a0e6eb77bce9306fc3f56fd3edbaeb74a356
F.I. Pelupessy <pelupes@strw.leidenuniv.nl>
2012-08-30 13:09:42 +0000

    update of example
    git-svn-id: http://www.amusecode.org/svn/trunk/sandbox@3953 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- pelupes/radhydro/radhydro2.py
+++ pelupes/radhydro/radhydro2.py
@@ -12,28 +12,35 @@ from amuse.community.sphray.interface import SPHRay
 from amuse.community.fi.interface import Fi
 from amuse.community.gadget2.interface import Gadget2
 
-from radhydro_code import RadiativeHydro
+from radiativehydro import RadiativeHydro
 
 from amuse.datamodel import Particles
 
 from amuse.support.io import write_set_to_file 
 
+from amuse.ext.evrard_test import body_centered_grid_unit_cube
+
 suggested_parameter_set=dict()
 
 suggested_parameter_set[Fi]=dict( radiation_flag=False,
                                   self_gravity_flag=False,
-                                  gamma=1,
-                                  isothermal_flag=True,
-                                  integrate_entropy_flag=False )
+#                                  gamma=1,
+#                                  isothermal_flag=True,
+                                  integrate_entropy_flag=True )
 
 suggested_parameter_set[Gadget2]=dict( )
 
 suggested_parameter_set[SimpleXSplitSet]=dict(number_of_freq_bins=5,
                                               thermal_evolution_flag=1,
-                                              blackbody_spectrum_flag=1)
+                                              blackbody_spectrum_flag=1,
+                                              metal_cooling_flag=0)
 suggested_parameter_set[SPHRay]=dict(default_spectral_type=1,
                                      number_of_rays=1000 | units.Myr**-1,
                                      isothermal_flag=False)
+
+import logging
+#logging.basicConfig(level=logging.DEBUG)
+
 
 def iliev_test_5( N=10000,
                   L=15. | units.kpc,
@@ -46,11 +53,26 @@ def iliev_test_5( N=10000,
   gamma=5./3.
   mu=1.| units.amu
 
+  
+  x,y,z=body_centered_grid_unit_cube(N).make_xyz()
+  N=len(x)
+  print N
   gas=Particles(N)
+
 # set particles homogeneously in space
-  gas.x=L*numpy.random.uniform(-1.,1.,N)
-  gas.y=L*numpy.random.uniform(-1.,1.,N)
-  gas.z=L*numpy.random.uniform(-1.,1.,N)
+#  gas.x=L*numpy.random.uniform(-1.,1.,N)
+#  gas.y=L*numpy.random.uniform(-1.,1.,N)
+#  gas.z=L*numpy.random.uniform(-1.,1.,N)
+
+  gas.x=L*x
+  gas.y=L*y
+  gas.z=L*z
+
+  gas.x+=L*numpy.random.uniform(-1.,1.,N)/1000
+  gas.y+=L*numpy.random.uniform(-1.,1.,N)/1000
+  gas.z+=L*numpy.random.uniform(-1.,1.,N)/1000
+
+  
 # set other properties
   gas.h_smooth = 0. | units.parsec
   gas.vx = 0. | (units.km/units.s)
@@ -59,8 +81,8 @@ def iliev_test_5( N=10000,
   gas.u = 1/(gamma-1)*constants.kB * Tinit/mu
   gas.rho = rhoinit
   gas.mass = rhoinit*(2*L)**3/N
-  gas.xion = 0.1 | units.none
-  gas.dudt=gas.u/(1.| units.Myr)
+  gas.xion = 0.1
+  gas.metallicity=0.02
 
 # set source in the center of the box
   sources=Particles(1)
@@ -95,7 +117,7 @@ def main( tend=30 | units.Myr,
     return hydro_code(converter, mode='periodic')
   
   def radcode():
-    return rad_code()
+    return rad_code()#redirection="none")
   
   radhydro=RadiativeHydro(rad=radcode,hydro=hydrocode)
   
@@ -108,11 +130,13 @@ def main( tend=30 | units.Myr,
       
   rad_parameters=suggested_parameter_set[rad_code]
   rad_parameters["box_size"]=2*L
-  rad_parameters["momentum_kicks_flag"]=False
+#  rad_parameters["momentum_kicks_flag"]=False
   for x in rad_parameters:
     radhydro.rad_parameters.__setattr__(x,rad_parameters[x])
   radhydro.rad_particles.add_particles(gas)
-#  radhydro.src_particles.add_particles(src)
+  radhydro.src_particles.add_particles(src)
+
+#  radhydro.constant_heating=gas.u/(20.| units.Myr)
 
   t=radhydro.model_time  
   while t<tend-dt/2:
@@ -121,6 +145,7 @@ def main( tend=30 | units.Myr,
 
     T=T_from_u(radhydro.rad_particles.xion,radhydro.gas_particles.u)
     xion=radhydro.rad_particles.xion
+    rho=radhydro.gas_particles.rho
     v=(radhydro.gas_particles.vx**2+radhydro.gas_particles.vz**2+radhydro.gas_particles.vy**2)**0.5
     print t.in_(units.Myr),
     print "min T:", T.amin().in_(units.K),
@@ -129,9 +154,11 @@ def main( tend=30 | units.Myr,
     print "max x_ion:", xion.max(),
     print "min v:", v.amin().in_(units.kms),
     print "max v:", v.amax().in_(units.kms)
+    print "min rho:", rho.amin().in_(units.amu/units.cm**3),
+    print "max rho:", rho.amax().in_(units.amu/units.cm**3)
     
   write_set_to_file(radhydro.radhydro_particles_copy(),'gas_final',"amuse", append_to_file=False)
     
 if __name__=="__main__":
-    main(hydro_code=Fi,rad_code=SPHRay,Tinit=10000. | units.K) # rad_code=SimpleXSplitSet
-  
+    main(hydro_code=Fi,rad_code=SPHRay,Tinit=100. | units.K,tend=50.| units.Myr,
+          rhoinit=.001 | (units.amu / units.cm**3) ) # rad_code=SimpleXSplitSet
---
+++ pelupes/radhydro/radiativehydro.py
@@ -0,0 +1,90 @@
+from amuse.community.fi.interface import Fi
+from amuse.community.gadget2.interface import Gadget2
+from amuse.datamodel import Particles
+
+"""
+class for radiative hydrodynamics
+
+"""
+
+
+class RadiativeHydro(object):
+    
+    def __init__(self, rad=None,hydro=None,src_particles=None):
+        if rad is None:
+          raise Exception("provide with rad class")
+        if hydro is None:
+          raise Exception("provide with hydro (sph) class")
+        self.rad=rad()
+        self.hydro=hydro()
+        self._src_particles=src_particles
+
+        self.hydro_parameters=self.hydro.parameters
+        self.rad_parameters=self.rad.parameters
+        self.rad_particles=self.rad.gas_particles
+        self.src_particles=self.rad.src_particles
+        self.gas_particles=self.hydro.gas_particles
+        self.particles=self.hydro.particles
+        self.dm_particles=self.hydro.dm_particles
+        try:
+          self.star_particles=self.hydro.star_particles
+        except:
+          self.star_particles=None
+        self.timestep=None
+        self.constant_heating=None
+
+    @property
+    def model_time(self):
+        return self.hydro.model_time
+
+    def evolve_model(self,tend):
+  
+        if self._src_particles:
+          self._src_particles.synchronize_to(self.rad.src_particles)
+          self._src_particles.new_channel_to(self.rad.sr_particles).copy_attributes(["x","y","z","luminosity"])
+  
+        self.hydro.gas_particles.synchronize_to(self.rad.gas_particles)
+    
+        update_rad_from_hydro_channel = self.hydro.gas_particles.new_channel_to(self.rad.gas_particles)
+        update_hydro_from_rad_channel = self.rad.gas_particles.new_channel_to(self.hydro.gas_particles)
+
+        rad_attributes_to_update,hydro_attributes_to_update=self.attributes_to_update()
+
+        if self.constant_heating is not None:
+          self.rad.gas_particles.du_dt=self.constant_heating
+    
+        t=self.model_time
+        timestep=self.timestep
+        if timestep is None:
+          timestep=tend-t
+    
+        while t < (tend-timestep/2):
+          self.hydro.evolve_model(t+timestep/2.)
+          update_rad_from_hydro_channel.copy_attributes(rad_attributes_to_update)
+          self.rad.evolve_model(t+timestep)
+          update_hydro_from_rad_channel.copy_attributes(hydro_attributes_to_update)
+          self.hydro.evolve_model(t+timestep)
+          t=self.model_time
+
+    def attributes_to_update(self):
+        rad_attributes_to_update=["x","y","z","rho","h_smooth"]
+        hydro_attributes_to_update=["u"]
+        
+        if hasattr(self.rad.parameters,"momentum_kicks_flag"):
+            momentum_kicks=self.rad.parameters.momentum_kicks_flag
+            if momentum_kicks:
+              rad_attributes_to_update.extend(["vx","vy","vz"])
+              hydro_attributes_to_update.extend(["vx","vy","vz"])
+          
+        if hasattr(self.hydro.parameters,"isothermal_flag"):
+          isothermal_flag=self.hydro.parameters.isothermal_flag
+          if isothermal_flag and self.constant_heating is None:
+            rad_attributes_to_update.append("du_dt")
+
+        return rad_attributes_to_update,hydro_attributes_to_update
+
+    def radhydro_particles_copy(self):
+          parts=self.rad.gas_particles.copy()
+          channel = self.gas_particles.new_channel_to(parts)
+          channel.copy_attributes(["mass","x","y","z","vx","vy","vz","rho","u"])  	  
+          return parts
