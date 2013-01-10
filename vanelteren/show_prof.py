277918fe8d8edd4992a7a0c00eef28b452484c6b
Arjen van Elteren <vanelteren@strw.leidenuniv.nl>
2013-01-10 15:12:52 +0000

    speed testing amuse
    git-svn-id: http://www.amusecode.org/svn/trunk/sandbox@4222 6e832d89-e7d1-4ad1-8ca1-873beecccf65

---
+++ vanelteren/show_prof.py
@@ -0,0 +1,14 @@
+import profile
+import pstats
+import sys
+
+if __name__ == '__main__':
+    stats = pstats.Stats(sys.argv[1])
+    stats.strip_dirs()
+    stats.sort_stats('cumulative')
+    
+    print 'INCOMING CALLERS:'
+    stats.print_callers()
+    
+    print 'OUTGOING CALLEES:'
+    stats.print_callees()
