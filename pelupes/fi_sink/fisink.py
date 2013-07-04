c2da53458ff42ba4d112351dd6607b61ed211ba7
F.I. Pelupessy <pelupes@strw.leidenuniv.nl>
2013-07-04 08:43:27 +0000

    new, updated and bug fixed fisink
    git-svn-id: http://www.amusecode.org/svn/trunk/sandbox@4714 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- pelupes/fi_sink/fisink.py
+++ pelupes/fi_sink/fisink.py
@@ -1,7 +1,7 @@
 from amuse.units import nbody_system
 from amuse.units import units
 
-import amuse.datamodel as core
+from amuse.datamodel import Particle,Particles
 from amuse.community.fi.interface import Fi
 
 from amuse.ext.sink import SinkParticles
@@ -38,37 +38,82 @@ note hard coded factor 2 (appropiate for fi: sink radius is smoothing kernel siz
 
 """
 
-
-
 class SinkFi(Fi):
     def __init__(self, *args, **kargs):
-        Fi.__init__(self, *args, **kargs)
-        self.sinks=core.Particles()
+        self.sink_particles=Particles()
         if not kargs.has_key('density_threshold'):
-          raise Exception("provide density threshold")
-        self.density_threshold=kargs['density_threshold']
-    
-    def evolve_model(self, *args, **kargs):
+          raise Exception("provide density_threshold")
+        self.density_threshold=kargs.pop('density_threshold')
+        if not kargs.has_key('merge_radius'):
+          raise Exception("provide merge_radius")
+        self.merge_radius=kargs.pop('merge_radius')
+        Fi.__init__(self, *args, **kargs)
+
+    def merge_sinks(self):
+        print "identify groups.." 
+        ccs=self.sink_particles.copy().connected_components(threshold=self.merge_radius)
+        if len(ccs):
+            print "merging sink sets... "
+        nmerge=0
+        newsinks=Particles()
+        for cc in ccs:
+            if len(cc) >1:
+              nmerge+=1
+              if len(cc) > 3: print "warning: large merge(than 3) ", len(cc)
+              new=Particle()
+              cc=cc.copy()
+              self.sink_particles.remove_particles(cc)
+              try:
+                new.radius=cc.sink_radius.max()
+              except Exception as ex:
+                print cc.__class__.__name__
+                print dir(cc)
+                print cc.sink_radius.__class__.__name__
+                print dir(cc.sink_radius)
+                raise ex 
+              new.mass=cc.total_mass()
+              new.position=cc.center_of_mass()
+              new.velocity=cc.center_of_mass_velocity()
+              cc.move_to_center()
+              new.angular_momentum=cc.total_angular_momentum()
+              new.sink_radius=new.radius
+              newsinks.add_particle(new)
+        if len(newsinks)>0:
+            new_in_code = self.dm_particles.add_particles(newsinks)
+            self.sink_particles.add_sink(new_in_code)
+        if nmerge>0: print "nmerg found: ",nmerge,
+        print "...done"
+
+    def commit_parameters(self):
         self.parameters.stopping_condition_maximum_density = self.density_threshold
+        self.overridden().commit_parameters()
+
+    def evolve_model(self, *args, **kargs):
         density_limit_detection = self.stopping_conditions.density_limit_detection
         density_limit_detection.enable()
-        self.overridden().evolve_model(*args,**kargs)        
+        self.overridden().evolve_model(*args,**kargs)
         while density_limit_detection.is_set():
-#          highdens = density_limit_detection.particles().copy_to_memory()
-          highdens=self.gas_particles.select_array(lambda rho:rho> self.density_threshold,["rho"]).copy()
-          self.gas_particles.remove_particles(highdens)
-          highdens.radius*=2
-          if len(self.sinks)>0:
-            self.sinks.accrete(highdens)
-            if len(highdens)>0:
-              highdens_in_code = self.dm_particles.add_particles(highdens)
-              self.sinks.add_sinks(highdens_in_code)
+          highdens=self.gas_particles.select_array(lambda rho:rho> self.density_threshold,["rho"])
+          candidate_sinks=highdens.copy()
+          if len(candidate_sinks)==0:
+            print "WARNING: unexpected no highdens"
+            raise Exception("I better stop")
+          candidate_sinks.radius*=2 
+          if len(self.sink_particles)>0:
+            self.sink_particles.accrete(candidate_sinks)
+            self.gas_particles.remove_particles(highdens)
+            if len(candidate_sinks)>0:
+              newsinks_in_code = self.dm_particles.add_particles(candidate_sinks)
+              self.sink_particles.add_sinks(newsinks_in_code)
           else:
-            highdens_in_code = self.dm_particles.add_particles(highdens)
-            self.sinks=SinkParticles(highdens_in_code)
+            self.gas_particles.remove_particles(highdens)
+            newsinks_in_code = self.dm_particles.add_particles(candidate_sinks)
+            self.sink_particles=SinkParticles(newsinks_in_code,looping_over="sources")
           self.overridden().evolve_model(*args,**kargs)
-          
-    def reinit_sink(self,sinks):
-        self.sinks=SinkParticles(sinks.get_intersecting_subset_in(self.dm_particles),sink_radius=sinks.sink_radius)
-
-
+        if len(self.sink_particles)>1: self.merge_sinks()        
+  
+    def reinit_sink(self,sink_particles):
+        sinks_in_code=sink_particles.get_intersecting_subset_in(self.dm_particles)
+        if len(sinks_in_code) != len(sink_particles):
+          raise Exception("all sinks need to be in dm_particles")
+        self.sink_particles=SinkParticles(sinks_in_code,sink_radius=sink_particles.sink_radius,looping_over="sources")
