fd71b6784ce0febe5378fbb9f6defc0d30c5b07c
Arjen van Elteren <vanelteren@strw.leidenuniv.nl>
2013-07-08 17:55:36 +0000

    small changes
    git-svn-id: http://www.amusecode.org/svn/trunk/sandbox@4722 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- vanelteren/multiples/show_plummer.py
+++ vanelteren/multiples/show_plummer.py
@@ -60,10 +60,10 @@ if  __name__ == '__main__':
     background.start()
     
     scale =  1 | nbody_system.length
-    figure = pyplot.figure()
+    figure = pyplot.figure(figsize=(5,5))
     plot = figure.add_subplot(111)
-    plot.set_xlim(-1,1)
-    plot.set_ylim(-1,1)
+    plot.set_xlim(-2,2)
+    plot.set_ylim(-2,2)
     x0, y0 = queue.get()
     scatter = plot.scatter(
         x0 / scale,
