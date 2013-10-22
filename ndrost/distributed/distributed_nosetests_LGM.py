d9ce57d8b2e29c6edbc6e73a79bf52e5d64916bd
Nathan de Vriesn <devries.nathan@gmail.com>
2013-10-22 12:03:02 +0000

    Added test-wrapper for Distributed AMUSE on LGM
    git-svn-id: http://www.amusecode.org/svn/trunk/sandbox@4885 6e832d89-e7d1-4ad1-8ca1-873beecccf65

---
+++ ndrost/distributed/distributed_nosetests_LGM.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+import nose
+from amuse.lab import *
+from interface import DistributedAmuseInterface
+from interface import DistributedAmuse
+from interface import Resource, Resources, Reservation, Reservations
+
+print "Setting up distributed code"
+instance = DistributedAmuse(redirection='none')
+instance.initialize_code()
+
+resource = Resource()
+resource.name='LGM'
+#~resource.location="vriesn@node00"
+#~resource.gateway="vriesn@fs.lgm.liacs.nl"
+resource.location="vriesn@fs.lgm.liacs.nl"
+resource.scheduler_type="ssh"
+resource.amuse_dir="/home/vriesn/amuse-svn"
+instance.resources.add_resource(resource)
+
+resource = Resource()
+resource.name="LGMnode00"
+resource.location="vriesn@node00"
+resource.gateway="vriesn@fs.lgm.liacs.nl"
+resource.scheduler_type="ssh"
+resource.amuse_dir="/home/vriesn/amuse-svn"
+instance.resources.add_resource(resource)
+print "Resources:"
+print instance.resources
+
+reservation = Reservations(2)
+reservation.resource_name=['local', 'LGMnode00']
+reservation.node_count=1
+reservation.time= 2|units.hour
+reservation.slots_per_node=10
+reservation.node_label=['local', 'LGMnode00']
+print reservation
+#~instance.reservations.add_reservation(reservation[0])
+instance.reservations.add_reservation(reservation[1])
+print "Reservations:"
+print instance.reservations
+
+print "Waiting for reservations"
+instance.wait_for_reservations()
+
+print "Running tests"
+
+nose.run()
+
+print "all tests done, stopping distributed code"
+
+instance.stop()
