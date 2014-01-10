c51d966466a09e36447ae116302bffaf97b96d1e
Steven Rieder <steven@stevenrieder.nl>
2014-01-10 15:34:05 +0000

    Modifications to gadget2 to allow multiple particle types with different softening lengths (work in progress).
    git-svn-id: http://www.amusecode.org/svn/trunk/sandbox@5032 6e832d89-e7d1-4ad1-8ca1-873beecccf65

---
+++ rieder/gadget2/interface.h
@@ -0,0 +1,33 @@
+extern "C" {
+    #include "src/allvars.h"
+    #include "src/proto.h"
+    #include "src/tags.h"
+    #include <gsl/gsl_rng.h>
+}
+
+typedef struct {
+    double mass;                                        /// mass
+    double x, y, z;                                     /// position
+    double vx, vy, vz;                                  /// velocity
+    int    ptype;                                       /// gadget particle type
+} dynamics_state;
+
+typedef struct {
+    double mass;                                        /// mass
+    double x, y, z;                                     /// position
+    double vx, vy, vz;                                  /// velocity
+    double u;                                           /// entropy
+} sph_state;
+
+
+extern "C" void   begrun(void);
+extern "C" double second(void);
+
+int found_particle(int index_of_the_particle, int *local_index);
+void update_particle_map(void);
+
+extern "C" void hydro_state_at_point(FLOAT pos[3], FLOAT vel[3], FLOAT *h_out,
+  FLOAT *ngb_out, FLOAT *dhsml_out, FLOAT *rho_out, FLOAT *rhov_out,
+  FLOAT *rhov2_out, FLOAT *rhoe_out);
+
+
