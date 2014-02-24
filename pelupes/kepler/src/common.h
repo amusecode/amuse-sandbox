ef87fe0a89550c7b2dab35862ae1e59ef66cd659
Guilherme Ferrari <ferrari@strw.leidenuniv.nl>
2014-02-24 15:33:55 +0000

    more improvements on Kepler solver: hopefully it is fast enough now.
    git-svn-id: http://www.amusecode.org/svn/trunk/sandbox@5154 6e832d89-e7d1-4ad1-8ca1-873beecccf65

---
+++ pelupes/kepler/src/common.h
@@ -0,0 +1,13 @@
+#ifndef __COMMON_H__
+#define __COMMON_H__
+
+#include <stdio.h>
+
+#define CONFIG_USE_DOUBLE
+
+typedef double REAL;
+typedef long INT;
+
+#define PI ((REAL)(3.141592653589793))
+
+#endif // __COMMON_H__
--- pelupes/kepler/test_kepler.py
+++ pelupes/kepler/test_kepler.py
@@ -21,11 +21,11 @@ def elements(starmass,x,y,z,vx,vy,vz,G=constants.G):
     mu=G*starmass
     r=(x**2+y**2+z**2)**0.5
     v2=(vx**2+vy**2+vz**2)
-    
+
     e=v2/2-mu/r
-    
+
     a=-mu/2/e
-    
+
     hx=y*vz-z*vy
     hy=z*vx-x*vz
     hz=x*vy-y*vx
@@ -36,53 +36,53 @@ def elements(starmass,x,y,z,vx,vy,vz,G=constants.G):
     ey=v2*y/mu-rdotv*vy/mu-y/r
     ez=v2*z/mu-rdotv*vz/mu-z/r
 
-    h2=(hx**2+hy**2+hz**2)    
-    
+    h2=(hx**2+hy**2+hz**2)
+
     eps=(1-h2/mu/a)**0.5
-    
+
     return a,eps
 
 def test_kepler( N,tend=1.| units.yr,method=0):
 
   numpy.random.seed(1234567)
-  
+
   conv=nbody_system.nbody_to_si(2.| units.MSun, 5.|units.AU)
-  
+
   comets=new_plummer_model(N,conv)
-  
+
   sun=Particle(mass=1.|units.MSun)
-  
+
   sun.position=[0,0,0]|units.AU
   sun.velocity=[0,0,0]|units.kms
-  
+
   comets.mass*=0.
-  
+
   code=Kepler(conv,redirection="none")
-  
+
   code.set_method(method)
-  
+
   code.central_particle.add_particle(sun)
   code.orbiters.add_particles(comets)
-  
+
   a0,eps0=elements(sun.mass,code.orbiters.x,code.orbiters.y,code.orbiters.z,
                      code.orbiters.vx,code.orbiters.vy,code.orbiters.vz)
-  
-  
+
+
 #  print code.orbiters.x[0]
   print orbital_elements_from_binary(code.particles[0:2],constants.G)
-  
+
   t1=time.time()
   code.evolve_model(tend)
   t2=time.time()
-  
+
   print orbital_elements_from_binary(code.particles[0:2],constants.G)
 #  print code.orbiters.x[0]
-  
-  
-  
+
+
+
   a,eps=elements(sun.mass,code.orbiters.x,code.orbiters.y,code.orbiters.z,
                      code.orbiters.vx,code.orbiters.vy,code.orbiters.vz)
-  
+
   da=abs((a-a0)/a0)
   deps=abs(eps-eps0)/eps0
 
@@ -94,32 +94,32 @@ def test_kepler( N,tend=1.| units.yr,method=0):
 
   pyplot.plot(a0[dev].value_in(units.AU),eps0[dev],"ro")
   pyplot.plot(a[dev].value_in(units.AU),eps[dev],"g+")
-  
-  
+
+
   print "max da,deps:",da.max(), deps.max()
-  
+
   print "time:",t2-t1
 
   pyplot.show()
-  
+
   return t2-t1,da.max(),deps.max()
-  
+
 def test_kepler_almost_parabolic( tend=1,method=0):
   code=Kepler(redirection="none")
-  
+
   code.set_method(method)
-  
+
   mass1=1.| nbody_system.mass
   mass2=0| nbody_system.mass
   semimajor_axis=1.|nbody_system.length
-  eccentricity=0.999999
+  eccentricity=0.9999999
   p=2*numpy.pi*(semimajor_axis**3/nbody_system.G/mass1)**0.5
   tend=tend*p
   print tend
   parts=new_binary_from_orbital_elements(
           mass1,
           mass2,
-          semimajor_axis, 
+          semimajor_axis,
           eccentricity = eccentricity,
           true_anomaly = 0.0102121
       )
@@ -129,35 +129,31 @@ def test_kepler_almost_parabolic( tend=1,method=0):
 
   a0,eps0=elements(mass1,code.orbiters.x,code.orbiters.y,code.orbiters.z,
                      code.orbiters.vx,code.orbiters.vy,code.orbiters.vz,G=nbody_system.G)
-  
-  
+
   print orbital_elements_from_binary(code.particles[0:2])
-  
+
   t1=time.time()
   code.evolve_model(tend)
   t2=time.time()
 
-  print "time:",t2-t1
-
-  
   print orbital_elements_from_binary(code.particles[0:2])
-  
+
   print code.orbiters.position
 
-  
   a,eps=elements(mass1,code.orbiters.x,code.orbiters.y,code.orbiters.z,
                      code.orbiters.vx,code.orbiters.vy,code.orbiters.vz,G=nbody_system.G)
-  
+
   da=abs((a-a0)/a0)
   deps=abs(eps-eps0)/eps0
 
   print da,deps
+  print "time:",t2-t1
 
-def test_kepler_parabolic( tend=1,method=0):
+def test_kepler_parabolic( tend=1,method=0, sign=+1):
   code=Kepler(redirection="none")
-  
+
   code.set_method(method)
-  
+
   sun=Particle()
   sun.mass=1. | nbody_system.mass
   sun.x=0. | nbody_system.length
@@ -173,9 +169,9 @@ def test_kepler_parabolic( tend=1,method=0):
   comet.y=0. | nbody_system.length
   comet.z=0. | nbody_system.length
   comet.vx=0. | nbody_system.speed
-  comet.vy=1.0000000001*(2*nbody_system.G*sun.mass/comet.x)**0.5 
+  comet.vy=(1.0 + sign * 1.0e-10)*(2*nbody_system.G*sun.mass/comet.x)**0.5
   comet.vz=0. | nbody_system.speed
-  
+
   tend=tend | nbody_system.time
   print tend
 
@@ -184,55 +180,53 @@ def test_kepler_parabolic( tend=1,method=0):
 
   a0,eps0=elements(sun.mass,code.orbiters.x,code.orbiters.y,code.orbiters.z,
                      code.orbiters.vx,code.orbiters.vy,code.orbiters.vz,G=nbody_system.G)
-  
+
   print orbital_elements_from_binary(code.particles[0:2])
-  
+
   t1=time.time()
   code.evolve_model(tend)
   t2=time.time()
 
-  print "time:",t2-t1
-
   print orbital_elements_from_binary(code.particles[0:2])
-  
+
   print code.orbiters.position
-  
+
   a,eps=elements(sun.mass,code.orbiters.x,code.orbiters.y,code.orbiters.z,
                      code.orbiters.vx,code.orbiters.vy,code.orbiters.vz,G=nbody_system.G)
-  
+
   da=abs((a-a0)/a0)
   deps=abs(eps-eps0)/eps0
 
   print da,deps
+  print "time:",t2-t1
 
 
-  
+
 if __name__=="__main__":
-  print "-"*10
-  print
+  tend = 1.0e9
 
   for method in [1,0]:
-    test_kepler_parabolic(tend=100000,method=method)
+    test_kepler_parabolic(tend=tend,method=method, sign=+1)
     print
 
   print "-"*10
   print
 
   for method in [1,0]:
-    test_kepler_almost_parabolic(tend=100000,method=method)
+    test_kepler_parabolic(tend=tend,method=method, sign=-1)
     print
 
   print "-"*10
   print
 
   for method in [1,0]:
-    test_kepler(N=100000,tend=1.e10| units.yr,method=method)  
+    test_kepler_almost_parabolic(tend=tend,method=method)
     print
 
-#  for method in [0,1]:
-#    test_kepler(N=100000,tend=1000.| units.yr,method=method)  
+  print "-"*10
+  print
 
+  for method in [1,0]:
+    test_kepler(N=100000,tend=tend | units.yr,method=method)
+    print
 
-#  for method in [0,1]:
-#    test_kepler(N=100,tend=1e6| units.yr,method=method)  
-  
