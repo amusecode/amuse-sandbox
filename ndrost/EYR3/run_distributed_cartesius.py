ee619c168fc4925e58ba23b898360f18c7da316d
Niels Drost <n.drost@esciencecenter.nl>
2014-03-07 17:29:56 +0000

    new distributed refactor
    git-svn-id: http://www.amusecode.org/svn/trunk/sandbox@5180 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- ndrost/EYR3/run_distributed_cartesius.py
+++ ndrost/EYR3/run_distributed_cartesius.py
@@ -18,7 +18,7 @@ from amuse.community.bhtree.interface import BHTree
 from amuse.community.phiGRAPE.interface import PhiGRAPE
 from amuse.community.ph4.interface import ph4
 from amuse.community.fastkick.interface import FastKick
-from amuse.community.distributed.interface import DistributedAmuse, Resource, Reservation
+from amuse.community.distributed.interface import DistributedAmuse, Resource, Pilot
 from SSEplus import SSEplus
 import cProfile
 
@@ -37,52 +37,52 @@ def new_lgm_node(node_name):
     resource.amuse_dir = "/home/niels/amuse"
     return resource
 
-def new_local_reservation():
-    reservation = Reservation()
-    reservation.resource_name = "local"
-    reservation.node_count = 1
-    reservation.time = 2 | units.hour
-    reservation.slots_per_node = 10
-    reservation.node_label = "local"
-    return reservation
+def new_local_pilot():
+    pilot = Pilot()
+    pilot.resource_name = "local"
+    pilot.node_count = 1
+    pilot.time = 2 | units.hour
+    pilot.slots_per_node = 10
+    pilot.node_label = "local"
+    return pilot
 
 def new_cartesius_resource():
     resource = Resource()
     resource.name = "cartesius"
     resource.location = "ndrosta@int2-bb.cartesius.surfsara.nl"
-    resource.amuse_dir = "/home/ndrosta/amuse"
+    resource.amuse_dir = "/home/ndrosta/amuse-svn"
     resource.scheduler_type = "slurm"
 #  
     return resource
 
 
-def new_cartesius_reservation():
-    reservation = Reservation()
-    reservation.resource_name = "cartesius"
-    reservation.node_count = 1
-    reservation.time = 1 | units.hour
-    reservation.queue_name = "short"
-    reservation.slots_per_node = 24
-    reservation.node_label = "cartesius"
-    return reservation
+def new_cartesius_pilot():
+    pilot = Pilot()
+    pilot.resource_name = "cartesius"
+    pilot.node_count = 2
+    pilot.time = 1 | units.hour
+    pilot.queue_name = "short"
+    pilot.slots_per_node = 24
+    pilot.node_label = "cartesius"
+    return pilot
 
-def new_gpu_node_reservation(resource):
-    reservation = Reservation()
-    reservation.resource_name = resource.name
-    reservation.node_count = 1
-    reservation.time = 2 | units.hour
-    reservation.slots_per_node = 2
-    reservation.node_label = "GPU"
-    return reservation
+def new_gpu_node_pilot(resource):
+    pilot = Pilot()
+    pilot.resource_name = resource.name
+    pilot.node_count = 1
+    pilot.time = 2 | units.hour
+    pilot.slots_per_node = 2
+    pilot.node_label = "GPU"
+    return pilot
 
-def new_cpu_node_reservation(resource):
-    reservation = Reservation()
-    reservation.resource_name = resource.name
-    reservation.node_count = 1
-    reservation.time = 2 | units.hour
-    reservation.slots_per_node = 10
-    reservation.node_label = "CPU"
-    return reservation
+def new_cpu_node_pilot(resource):
+    pilot = Pilot()
+    pilot.resource_name = resource.name
+    pilot.node_count = 1
+    pilot.time = 2 | units.hour
+    pilot.slots_per_node = 10
+    pilot.node_label = "CPU"
+    return pilot
 
 
 
@@ -92,25 +92,25 @@ def start_distributed(lgm_node_names):
     lgm_nodes = [new_lgm_node(lgm_node_name) for lgm_node_name in lgm_node_names]
     instance = DistributedAmuse(redirection="file", redirect_file="distributed_amuse.log")
     instance.initialize_code()
-    instance.resources.add_resource(new_lgm_gateway())
-    for lgm_node in lgm_nodes:
-        instance.resources.add_resource(lgm_node)
+    #instance.resources.add_resource(new_lgm_gateway())
+    #for lgm_node in lgm_nodes:
+    #    instance.resources.add_resource(lgm_node)
 
     instance.resources.add_resource(new_cartesius_resource())
     
-    instance.reservations.add_reservation(new_local_reservation())
-    for lgm_node in lgm_nodes:
-        instance.reservations.add_reservation(new_gpu_node_reservation(lgm_node))
-        instance.reservations.add_reservation(new_cpu_node_reservation(lgm_node))
+    instance.pilots.add_pilot(new_local_pilot())
+    #for lgm_node in lgm_nodes:
+    #    instance.pilots.add_pilot(new_gpu_node_pilot(lgm_node))
+    #    instance.pilots.add_pilot(new_cpu_node_pilot(lgm_node))
 
-    instance.reservations.add_reservation(new_cartesius_reservation())
+    instance.pilots.add_pilot(new_cartesius_pilot())
     
-    print "Reservations:"
-    print instance.reservations
-    print "Waiting for reservations"
-    instance.wait_for_reservations()
-    print "Reservations status after waiting:"
-    print instance.reservations
+    print "Pilots:"
+    print instance.pilots
+    print "Waiting for pilots"
+    instance.wait_for_pilots()
+    print "Pilots status after waiting:"
+    print instance.pilots
     return instance
 
 
@@ -132,17 +132,17 @@ if __name__ == "__main__":
             dt_plot = 0.010 | units.Myr,
             #~grav_code = PhiGRAPE,
             grav_code = ph4,
-            grav_code_extra = dict(node_label="GPU", mode="gpu", redirection="file", redirect_file="grav_code.log"),
+            grav_code_extra = dict(node_label="local", redirection="file", redirect_file="grav_code.log"),
             gas_code = Gadget2,
-            gas_code_extra = dict(node_label="cartesius", mode="nogravity", number_of_workers=2, redirection="file", redirect_file="gas_code.log"),
+            gas_code_extra = dict(node_label="cartesius", mode="nogravity", number_of_workers=20, redirection="file", redirect_file="gas_code.log"),
             se_code = SSEplus,
             se_code_extra = dict(redirection="file", redirect_file="se_code.log"),
             #~grav_couple_code = Octgrav,
             #~grav_couple_code_extra = dict(node_label="GPU", redirection="file", redirect_file="grav_couple_code.log")
-            grav_couple_code = FastKick,
-            grav_couple_code_extra = dict(node_label="GPU", mode="gpu", number_of_workers=1, redirection="file", redirect_file="grav_couple_code.log")
-            #~grav_couple_code = BHTree,
-            #~grav_couple_code_extra = dict(redirection="file", redirect_file="grav_couple_code.log")
+            #grav_couple_code = FastKick,
+            #grav_couple_code_extra = dict(node_label="GPU", mode="gpu", number_of_workers=1, redirection="file", redirect_file="grav_couple_code.log")
+            grav_couple_code = BHTree,
+            grav_couple_code_extra = dict(node_label="local", redirection="file", redirect_file="grav_couple_code.log")
         )
         cProfile.run("clustergas.clustergas(**kwargs)", "prof")
     finally:
