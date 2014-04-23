46e96b9e76462aee45a81af52241571fdec9d436
Maxwell Tsai <maxwellemail@gmail.com>
2014-04-23 15:40:14 +0000

    NBODY6++ testing script
    git-svn-id: http://www.amusecode.org/svn/trunk/sandbox@5350 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- maxwell/runnb6.py
+++ maxwell/runnb6.py
@@ -6,29 +6,38 @@ from amuse.lab import *
 from time import sleep
 
 if __name__=="__main__":
-    converter = nbody_system.nbody_to_si(1 | units.MSun, 1 | units.AU)
+    converter = nbody_system.nbody_to_si(700 | units.MSun, 1 | units.parsec)
 
 
     instance = Nbody6xx(converter,redirection="none")
     instance.initialize_code()
-    instance.particles.add_particles(new_plummer_model(1000, converter))
+    instance.set_kz(20,0)
+    pts = new_plummer_model(1000, converter)
+    pts.radius = 1 | units.RSun
+    pts.mass = new_salpeter_mass_distribution(len(pts.mass))
+    pts.scale_to_standard(convert_nbody=converter)
+    instance.set_rbar(1)
+    instance.set_zmbar(0.7)
+    instance.particles.add_particles(pts)
     #print "potential energy = ", instance.get_potential_energy()
     print "total mass = ", instance.get_total_mass()
     print "total radius = ", instance.get_total_radius()
-    t_end = 0.5| units.Myr
+    t_end = 0.1| units.Myr
     t=instance.get_time()
-    plt.ion()
+    #plt.ion()
     while (t<t_end):
-        instance.evolve_model(t_end)
+        instance.evolve_model(t)
         t=instance.get_time()
-        print "t = ", t | units.Myr
+        print "t = ", t.value_in(units.Myr)
         print "n = ", instance.get_number_of_particles()
         #print instance.particles.x.value_in(nbody_system.length)
-        plt.clf()
-        plt.scatter(instance.particles.x.value_in(units.AU),instance.particles.y.value_in(units.AU))
-        plt.draw()
-        plt.show()
+        #plt.clf()
+        #plt.scatter(instance.particles.x.value_in(units.AU),instance.particles.y.value_in(units.AU))
+        #plt.draw()
+        #plt.show()
+        print instance.get_time_step()
+        t += instance.get_time_step()
         sleep(0.1)
 
     instance.stop()
-    plt.close()
+    #plt.close()
