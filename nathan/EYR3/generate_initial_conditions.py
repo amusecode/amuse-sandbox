c0ad06314fa76461531962f32f1d8031736f34bf
Nathan de Vriesn <devries.nathan@gmail.com>
2014-05-09 11:43:17 +0000

    New version EYR3 sandbox scripts
    git-svn-id: http://www.amusecode.org/svn/trunk/sandbox@5382 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- nathan/EYR3/generate_initial_conditions.py
+++ nathan/EYR3/generate_initial_conditions.py
@@ -10,6 +10,7 @@ from amuse.ic.fractalcluster import new_fractal_cluster_model
 from amuse.ic.plummer import new_plummer_model
 from amuse.ext.spherical_model import new_gas_plummer_distribution
 from amuse.ext.relax_sph import relax
+from amuse.community.fi.interface import Fi
 from amuse.community.gadget2.interface import Gadget2
 from amuse.community.fastkick.interface import FastKick
 
@@ -23,10 +24,10 @@ def to_initial_conditions_directory():
 
 def generate_initial_conditions(
         number_of_stars = 10000,
-        number_of_gas_particles = 10**7,
+        number_of_gas_particles = 2*10**6,
         star_formation_efficiency = 0.1,
-        virial_radius = 1.0 | units.parsec,
-        virial_ratio = 0.5,
+        virial_radius = 0.33 | units.parsec,
+        virial_ratio = 1.0,
         use_fractal = False):
     
     numpy.random.seed(12345678)
@@ -58,21 +59,32 @@ def generate_initial_conditions(
     
     filename = "YSC_{0}_stars{1}_gas{2}k_".format("fractal" if use_fractal else "plummer",
         number_of_stars, number_of_gas_particles/1000)
-    write_set_to_file(stars, filename+"stars.amuse", "amuse")
-    write_set_to_file(gas, filename+"gas.amuse", "amuse")
+    print "Writing initial conditions to", filename, "+ stars/gas.amuse"
+    write_set_to_file(stars, filename+"stars.amuse", "amuse", append_to_file=False)
+    write_set_to_file(gas, filename+"gas.amuse", "amuse", append_to_file=False)
     with open(filename+"info.pkl", "wb") as outfile:
         cPickle.dump([converter], outfile)
     return stars, gas, filename
 
+def new_hydro(gas, dynamical_timescale, converter):
+    if False:
+        hydro = Gadget2(converter, number_of_workers=8, redirection="file", redirect_file="gadget.log")
+        hydro.parameters.time_max = 3 * dynamical_timescale
+        hydro.parameters.max_size_timestep = dynamical_timescale / 100
+        hydro.parameters.time_limit_cpu = 1.0 | units.Gyr
+    else:
+        hydro = Fi(converter, mode='openmp', redirection="file", redirect_file="fi.log")
+        hydro.parameters.timestep = dynamical_timescale / 100
+        hydro.parameters.eps_is_h_flag = True
+    return hydro
+    
 def relax_initial_conditions(stars, gas, filename):
     dynamical_timescale = gas.dynamical_timescale()
     converter = nbody_system.nbody_to_si(dynamical_timescale, 1|units.parsec)
-    hydro = Gadget2(converter, number_of_workers=2)
-    hydro.parameters.time_max = 3 * dynamical_timescale
-    hydro.parameters.max_size_timestep = dynamical_timescale / 100
-    hydro.parameters.time_limit_cpu = 1.0 | units.Gyr
+    hydro = new_hydro(gas, dynamical_timescale, converter)
     
-    gravity_field_code = FastKick(converter)
+    gravity_field_code = FastKick(converter, mode="gpu", number_of_workers=2)
+    gravity_field_code.parameters.epsilon_squared = (0.01 | units.parsec)**2
     gravity_field_code.particles.add_particles(stars)
     relaxed_gas = relax(gas, hydro, gravity_field=gravity_field_code, 
         monitor_func="energy",
@@ -84,5 +96,5 @@ def relax_initial_conditions(stars, gas, filename):
 if __name__ == "__main__":
     to_initial_conditions_directory()
     stars, gas, filename = generate_initial_conditions()
-    relax_initial_conditions(stars, gas, filename)
+#    relax_initial_conditions(stars, gas, filename)
 
--- nathan/EYR3/young_star_cluster_distributed.py
+++ nathan/EYR3/young_star_cluster_distributed.py
@@ -23,15 +23,16 @@ from gravity_hydro_stellar import GravityHydroStellar
 
 def simulate_young_star_cluster(distributed=None):
     end_time = 10.0 | units.Myr
-    end_time = 0.06 | units.Myr
+#    end_time = 0.004 | units.Myr
     time_step = 0.01 | units.Myr
     
     new_working_directory()
     
-    stars, gas, converter = load_young_star_cluster("YSC_stars1000_gas100k_")
-    #stars, gas, converter = load_young_star_cluster("YSC_stars1000_gas1000k_")
+    stars, gas, converter = load_young_star_cluster("YSC_plummer_stars10000_gas2000k_")
+#    stars, gas, converter = load_young_star_cluster("YSC_plummer_stars10000_gas10000k_")
+#    stars, gas, converter = load_young_star_cluster("YSC_plummer_stars1000_gas1000k_")
     system = new_gravity_hydro_stellar(stars, gas, converter, time_step, distributed)
-    system.store_system_state()
+#    system.store_system_state()
     system.evolve_model(end_time)
     system.stop()
 
@@ -56,8 +57,8 @@ def load_young_star_cluster(filename_base):
     with open(filename_base + "info.pkl", "rb") as infile:
         converter = cPickle.load(infile)[0]
     stars = read_set_from_file(filename_base + "stars.amuse", "amuse")
-#    gas = read_set_from_file(filename_base + "gas.amuse", "amuse")
-    gas = read_set_from_file(filename_base + "gas_relaxed.amuse", "amuse")
+    gas = read_set_from_file(filename_base + "gas.amuse", "amuse")
+#    gas = read_set_from_file(filename_base + "gas_relaxed.amuse", "amuse")
     return stars, gas, converter
 
 def new_gravity_hydro_stellar(stars, gas, converter, time_step, distributed):
@@ -67,8 +68,9 @@ def new_gravity_hydro_stellar(stars, gas, converter, time_step, distributed):
     
     epsilon_squared_bridge = (constants.G * stars.mass.max() * time_step**2)**(2.0/3.0)
     print "Gravitational softening for Bridge:", epsilon_squared_bridge.sqrt().as_quantity_in(units.parsec)
-    gravity_to_hydro = new_gravity_field_from_stars([gravity], converter, epsilon_squared_bridge)
-    hydro_to_gravity = new_gravity_field_from_gas([hydro], converter, epsilon_squared_bridge)
+    gravity_field_code = new_gravity_field_code(converter, epsilon_squared_bridge)
+    gravity_to_hydro = new_gravity_field_from_stars(gravity, gravity_field_code)
+    hydro_to_gravity = new_gravity_field_from_gas(hydro, gravity_field_code)
     
     system = GravityHydroStellar(
         gravity, hydro, stellar, 
@@ -80,7 +82,8 @@ def new_ph4_gravity(stars, converter, distributed):
     if distributed is None:
         distributed_kwargs = dict()
     else:
-        distributed_kwargs = dict(node_label="2GPUs_ph4", mode="gpu")
+#        distributed_kwargs = dict(label="2GPUs_ph4", mode="gpu")
+        distributed_kwargs = dict(label="GPU", mode="gpu")
     
     gravity = ph4(
         converter,
@@ -102,26 +105,41 @@ def new_gravity(code, *args, **kwargs):
 
 def new_hydro(gas, converter, time_step, distributed):
     if distributed is None:
-        distributed_kwargs = dict(number_of_workers=2)
+#        distributed_kwargs = dict(number_of_workers=24, label="hydro")
+        distributed_kwargs = dict(number_of_workers=16)
     else:
-        distributed_kwargs = dict(node_label="hydro", number_of_workers=24, number_of_nodes=1)
+#        distributed_kwargs = dict(label="local", number_of_workers=6, number_of_nodes=1)
+        distributed_kwargs = dict(label="hydro", number_of_workers=16, number_of_nodes=1)
         if "cartesius" in distributed.resources.name:
             distributed.pilots.add_pilot(new_cartesius_pilot())
             print "Waiting for Cartesius reservation"
             distributed.wait_for_pilots()
     
     print "Start hydro"
-    hydro = Gadget2(
-        converter,
-        redirection="file",
-        redirect_file="gas_code.log",
-        **distributed_kwargs)
-    hydro.parameters.gas_epsilon = converter.to_si(0.01|nbody_system.length) # or eps_is_h_flag
-    hydro.parameters.n_smooth = 64
-    hydro.parameters.n_smooth_tol = 0.005 |units.none
-    hydro.parameters.time_max = 32.0 | units.Myr
-    hydro.parameters.max_size_timestep = time_step
-    hydro.parameters.time_limit_cpu = 1.0 | units.yr
+    if True:
+        hydro = Gadget2(
+            converter,
+            redirection="file",
+            redirect_file="gas_code.log",
+            **distributed_kwargs)
+#        hydro.parameters.n_smooth = 64
+#        hydro.parameters.n_smooth_tol = 0.005 |units.none
+        hydro.parameters.time_max = 32.0 | units.Myr
+        hydro.parameters.max_size_timestep = time_step
+        hydro.parameters.time_limit_cpu = 1.0 | units.yr
+    else:
+        distributed_kwargs['number_of_threads'] = distributed_kwargs['number_of_workers']
+        distributed_kwargs['number_of_workers'] = 1
+        print distributed_kwargs
+        hydro = Fi(
+            converter, mode='openmp',
+            redirection="file",
+            redirect_file="gas_code.log",
+            **distributed_kwargs)
+        hydro.parameters.eps_is_h_flag = True
+        hydro.parameters.timestep = time_step
+        hydro.parameters.verbosity = 99
+
     hydro.gas_particles.add_particles(gas)
     hydro.commit_particles()
     eps = hydro.gas_particles.radius.as_quantity_in(units.parsec)
@@ -129,38 +147,32 @@ def new_hydro(gas, converter, time_step, distributed):
     return hydro
 
 def new_stellar(stars):
-    stellar = SSE(node_label="local", redirection="file", redirect_file="stellar.log")
+    stellar = SSE(label="local", redirection="file", redirect_file="stellar.log")
     stellar.particles.add_particles(stars)
     return stellar
 
-def new_gravity_field_from_stars(codes, converter, epsilon_squared):
-    gravity_field_code = Octgrav(
+def new_gravity_field_code(converter, epsilon_squared):
+    gravity_field_code = FastKick(
         converter,
-        node_label="GPU", 
+        mode="gpu",
+        label="GPU", 
         redirection="file", 
-        redirect_file="gravity_field_code_gravity_to_hydro.log")
+        redirect_file="gravity_field_code.log")
     gravity_field_code.parameters.epsilon_squared = epsilon_squared
-    gravity_field_code.parameters.opening_angle = 0.5
+    return gravity_field_code
     
+def new_gravity_field_from_stars(gravity_code, gravity_field_code):
     gravity_field = CalculateFieldForCodesUsingReinitialize(
         gravity_field_code, 
-        codes, 
+        [gravity_code], 
         required_attributes=['mass', 'x', 'y', 'z', 'vx', 'vy', 'vz', 'radius']
     ) # "radius" isn't really required, but get_state is faster than get_mass+get_position+get_velocity
     return gravity_field
 
-def new_gravity_field_from_gas(codes, converter, epsilon_squared):
-    gravity_field_code = Octgrav(
-        converter,
-        node_label="GPU", 
-        redirection="file", 
-        redirect_file="gravity_field_code_hydro_to_gravity.log")
-    gravity_field_code.parameters.epsilon_squared = epsilon_squared
-    gravity_field_code.parameters.opening_angle = 0.5
-    
+def new_gravity_field_from_gas(hydro_code, gravity_field_code):
     gravity_field = CalculateFieldForCodesUsingReinitialize(
         gravity_field_code, 
-        codes, 
+        [hydro_code], 
         required_attributes=['mass', 'x', 'y', 'z', 'vx', 'vy', 'vz', 'u']
     ) # "u" isn't really required, but get_state_sph is faster than get_mass+get_position+get_velocity
     return gravity_field
@@ -174,21 +186,24 @@ def new_lgm_gateway():
     resource.amuse_dir = "/home/vriesn/amuse-svn"
     return resource
 
-def new_lgm_node(node_name):
+def new_lgm_node(node_name, kepler=False):
     resource = Resource()
     resource.name = "LGM" + node_name
     resource.location = node_name
     resource.gateway = "LGM"
-    resource.amuse_dir = "/home/vriesn/amuse-svn"
+    if kepler:
+        resource.amuse_dir = "/home/vriesn/kepler/amuse-svn"
+    else:
+        resource.amuse_dir = "/home/vriesn/amuse-svn"
     return resource
 
-def new_local_pilot():
+def new_local_pilot(slots=10, label="local"):
     pilot = Pilot()
     pilot.resource_name = "local"
     pilot.node_count = 1
     pilot.time = 2 | units.hour
-    pilot.slots_per_node = 10
-    pilot.node_label = "local"
+    pilot.slots_per_node = slots
+    pilot.label = label
     return pilot
 
 def new_gpu_node_pilot(resource, slots=2, label="GPU"):
@@ -197,7 +212,7 @@ def new_gpu_node_pilot(resource, slots=2, label="GPU"):
     pilot.node_count = 1
     pilot.time = 2 | units.hour
     pilot.slots_per_node = slots
-    pilot.node_label = label
+    pilot.label = label
     return pilot
 
 def new_cpu_node_pilot(resource):
@@ -206,7 +221,7 @@ def new_cpu_node_pilot(resource):
     pilot.node_count = 1
     pilot.time = 2 | units.hour
     pilot.slots_per_node = 10
-    pilot.node_label = "CPU"
+    pilot.label = "CPU"
     return pilot
 
 def new_cartesius_resource():
@@ -224,7 +239,7 @@ def new_cartesius_pilot():
     pilot.time = 1 | units.hour
     pilot.queue_name = "short"
     pilot.slots_per_node = 24
-    pilot.node_label = "hydro"
+    pilot.label = "hydro"
     return pilot
 
 def new_hofvijver_resource():
@@ -240,31 +255,34 @@ def new_hofvijver_pilot():
     pilot.node_count = 1
     pilot.time = 2 | units.hour
     pilot.slots_per_node = 64
-    pilot.node_label = "hydro"
+    pilot.label = "hydro"
     return pilot
 
 
-def start_distributed(lgm_node_names, forhydro="none"):
-    lgm_nodes = [new_lgm_node(lgm_node_name) for lgm_node_name in lgm_node_names]
+def start_distributed_local():
     instance = DistributedAmuse(redirection="file", redirect_file="distributed_amuse.log")
     instance.initialize_code()
-    instance.resources.add_resource(new_lgm_gateway())
-    for lgm_node in lgm_nodes:
-        instance.resources.add_resource(lgm_node)
-    
-    for lgm_node in lgm_nodes:
-        instance.pilots.add_pilot(new_cpu_node_pilot(lgm_node))
-    instance.pilots.add_pilot(new_gpu_node_pilot(lgm_nodes[0], slots=1, label="2GPUs_ph4"))
-    instance.pilots.add_pilot(new_gpu_node_pilot(lgm_nodes[1]))
+    instance.parameters.debug = True
+    instance.use_for_all_workers()
     instance.pilots.add_pilot(new_local_pilot())
+    instance.pilots.add_pilot(new_local_pilot(slots=2, label="GPU"))
+    instance.pilots.add_pilot(new_local_pilot(slots=24, label="hydro"))
     
-    if forhydro == "hofvijver":
-        instance.resources.add_resource(new_hofvijver_resource())
-        instance.pilots.add_pilot(new_hofvijver_pilot())
-    elif forhydro == "cartesius":
-        instance.resources.add_resource(new_cartesius_resource())
-    else:
-        raise Exception("Unknown option for forhydro resource: '{0}'".format(forhydro))
+    print "Pilots:"
+    print instance.pilots
+    print "Waiting for pilots"
+    instance.wait_for_pilots()
+    return instance
+
+def start_distributed_cartesius():
+    instance = DistributedAmuse(redirection="file", redirect_file="distributed_amuse_cartesius.log")
+    instance.initialize_code()
+    instance.parameters.debug = True
+    instance.use_for_all_workers()
+
+    instance.resources.add_resource(new_cartesius_resource())
+    instance.pilots.add_pilot(new_local_pilot())
+    instance.pilots.add_pilot(new_local_pilot(slots=2, label="GPU"))
     
     print "Pilots:"
     print instance.pilots
@@ -275,19 +293,17 @@ def start_distributed(lgm_node_names, forhydro="none"):
 
 
 if __name__ == "__main__":
-#~    logging.basicConfig(filename='example.log', level=logging.WARN)
+#    logging.basicConfig(filename='logging.log', level=logging.WARN)
     logging.basicConfig(level=logging.WARN)
     logging.getLogger("code").setLevel(logging.DEBUG)
 
-    run_local = False
+    run_local = True
     if run_local is True:
         cProfile.run("simulate_young_star_cluster()", "prof")
     else:
-#~        distributed = start_distributed(lgm_node_names=["node05", "node07"], forhydro="cartesius")
-        distributed = start_distributed(lgm_node_names=["node05", "node07"], forhydro="hofvijver")
-        try:
-            cProfile.run("simulate_young_star_cluster(distributed)", "prof")
-        finally:
-            distributed.stop()
-            print "Done"
+        distributed = start_distributed_cartesius()
+#        distributed = start_distributed_local()
+        cProfile.run("simulate_young_star_cluster(distributed)", "prof")
+        distributed.stop()
+        print "Done"
 
