09caea367d18aa5c8ae6da5e665f4387a0a8e2ce
F.I. Pelupessy <pelupes@strw.leidenuniv.nl>
2014-06-20 09:12:43 +0000

    small fixes for gas galactics
    git-svn-id: http://www.amusecode.org/svn/trunk/sandbox@5478 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- pelupes/galactics/testgaslactics.py
+++ pelupes/galactics/testgaslactics.py
@@ -4,18 +4,18 @@ from amuse.units import units,nbody_system
 
 from matplotlib import pyplot
 
-convert = nbody_system.nbody_to_si(1.0 | units.kpc, 1.0e6 | units.MSun)
+convert = nbody_system.nbody_to_si(1.0 | units.kpc, 1.0e9 | units.MSun)
 
-instance = GaslactICs(unit_converter=convert,redirection="none")
+print convert.to_nbody(8 | units.kms)
+
+instance = GaslactICs(unit_converter=convert)
 
 #instance.parameters.halo_random_seed=12345
 #instance.parameters.bulge_number_of_particles=1000
 #instance.parameters.halo_number_of_particles=50
 #instance.parameters.disk_number_of_particles=1000
 instance.parameters.output_directory="./"
-print instance.parameters
-
-print instance.model_present()
+#print instance.parameters
 
 instance.commit_parameters()
 
@@ -27,16 +27,22 @@ bulge=instance.bulge_particles
 disk=instance.disk_particles
 gas=instance.gas_particles
 
-print halo
-
 print len(p)
 
 print p.total_mass().in_(units.MSun)
 
-pyplot.plot(halo.x.value_in(units.kpc),halo.z.value_in(units.kpc),'y.')
-pyplot.plot(bulge.x.value_in(units.kpc),bulge.z.value_in(units.kpc),'b.')
-pyplot.plot(disk.x.value_in(units.kpc),disk.z.value_in(units.kpc),'g.')
-pyplot.plot(gas.x.value_in(units.kpc),gas.z.value_in(units.kpc),'r.')
-pyplot.xlim(-30,30)
-pyplot.ylim(-30,30)
+print gas.u[0]**0.5
+
+#pyplot.plot(halo.x.value_in(units.kpc),halo.z.value_in(units.kpc),'y.')
+#pyplot.plot(bulge.x.value_in(units.kpc),bulge.z.value_in(units.kpc),'b.')
+#pyplot.plot(disk.x.value_in(units.kpc),disk.z.value_in(units.kpc),'g.')
+#pyplot.plot(gas.x.value_in(units.kpc),gas.z.value_in(units.kpc),'r.')
+#pyplot.xlim(-30,30)
+#pyplot.ylim(-30,30)
+
+r=(gas.x**2+gas.y**2)**0.5
+pyplot.plot(r.value_in(units.kpc),gas.z.value_in(units.kpc),'r.')
+pyplot.xlim(0,30)
+pyplot.ylim(-5,5)
+
 pyplot.show()
