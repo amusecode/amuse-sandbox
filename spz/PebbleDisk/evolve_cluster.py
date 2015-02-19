ac92038308eb01deebe4c0a54929dd122b01b690
Simon Portegies Zwart <spz@strw.leidenuniv.nl>
2015-02-19 14:24:06 +0000

    Added Pebble Disk
    git-svn-id: http://www.amusecode.org/svn/trunk/sandbox@5917 6e832d89-e7d1-4ad1-8ca1-873beecccf65

---
+++ spz/PebbleDisk/evolve_cluster.py
@@ -0,0 +1,273 @@
+import sys
+import numpy
+from numpy import random
+from amuse.lab import *
+from amuse.couple import bridge
+from make_pebble_disk import make_pebble_disk
+from amuse.units.optparse import OptionParser
+from matplotlib import pyplot
+from evolve_disk import *
+from make_planets import *
+
+Johannes = None
+
+def movie(stars):
+    m = 100*stars.mass/stars.mass.max()
+    colors = ['r', 'b', 'g']
+
+    pyplot.subplot(2,2,1)
+    pyplot.scatter(stars.x.value_in(units.AU), stars.y.value_in(units.AU), c='r', s=m)
+    for star, color in zip(stars, colors):
+        pyplot.scatter(star.disk_particles.x.value_in(units.AU), star.disk_particles.y.value_in(units.AU), c=color, s=1)
+    pyplot.xlabel("X [AU]")
+    pyplot.ylabel("Y [AU]")
+
+    pyplot.subplot(2,2,2)
+    pyplot.scatter(stars.x.value_in(units.AU), stars.z.value_in(units.AU), c='r', s=m)
+    for star, color in zip(stars, colors):
+        pyplot.scatter(star.disk_particles.x.value_in(units.AU), star.disk_particles.z.value_in(units.AU), c=color, s=1)
+    pyplot.xlabel("X [AU]")
+    pyplot.ylabel("Z [AU]")
+
+#    pyplot.xlim(-100, 100)
+#    pyplot.ylim(-100, 100)
+
+    pyplot.subplot(2,2,3)
+    pyplot.ylim(0, 1)
+    pyplot.semilogx()
+    for star, color in zip(stars, colors):
+        pyplot.scatter(star.disk_particles.semimajor_axis.value_in(units.AU), star.disk_particles.eccentricity, c=color, s=10, lw=0)
+    pyplot.xlabel("a [AU]")
+    pyplot.ylabel("e")
+
+    pyplot.subplot(2,2,4)
+    pyplot.ylim(0, 1)
+    pyplot.semilogx()
+    all_planets = Particles()
+    for star in stars:
+        all_planets.add_particles(star.planets)
+    mscale = 0.1*all_planets.mass.min()
+    for star, color in zip(stars, colors):
+        pyplot.scatter(star.planets.semimajor_axis.value_in(units.AU), star.planets.eccentricity, c=color, s=star.planets.mass/mscale, lw=0)
+    pyplot.xlabel("a [AU]")
+    pyplot.ylabel("e")
+
+    pyplot.draw()
+    pyplot.subplot(2,2,1)
+    pyplot.cla()
+    pyplot.subplot(2,2,2)
+    pyplot.cla()
+    pyplot.subplot(2,2,3)
+    pyplot.cla()
+    pyplot.subplot(2,2,4)
+    pyplot.cla()
+
+
+def identify_nearest_neighbor(stars, time):
+
+    for i, star in enumerate(stars):   
+         r = (star.position - stars.position).lengths()
+         r[r==(0|r.unit)] = r.max()
+         istar_min = r.argmin()
+         print istar_min
+         star.nearest_neighbor = stars[istar_min]
+         star.r_nn = r[istar_min]
+         star.t_nn = time
+
+def get_kepler_elements(star, pebble):
+    pos = star.position - pebble.position
+    vel = star.velocity - pebble.velocity
+    Johannes.initialize_from_dyn(star.mass + pebble.mass, pos[0], pos[1], pos[2], vel[0], vel[1], vel[2])
+    a, e = Johannes.get_elements()
+    return a, e
+
+def determine_orbital_parameters_of_pebbles(stars):
+    for star in stars:
+        for pebble in star.disk_particles:
+            if pebble.star is None:
+                continue
+            a, e = get_kepler_elements(star, pebble)
+            pebble.semimajor_axis = a
+            pebble.eccentricity = e
+
+def determine_orbital_parameters_of_planets(stars):
+    for star in stars:
+        for planet in star.planets:
+            if planet.star is None:
+                continue
+            a, e = get_kepler_elements(star, planet)
+            planet.semimajor_axis = a
+            planet.eccentricity = e
+
+def potential_energy(pebble, star):
+    r = (pebble.position-star.position).length()
+    return -constants.G*pebble.mass*star.mass/r
+
+def kinetic_energy(pebble, star):
+    return 0.5*pebble.mass * (pebble.velocity-star.velocity).length()**2
+
+def identify_host_star(pebble, stars):
+    bound_to_star = None
+    Eb_max = zero
+    for star in stars:
+        binding_energy = potential_energy(pebble, star) +  kinetic_energy(pebble, star)
+        if binding_energy<zero:
+            if binding_energy < Eb_max:
+                Eb_max = binding_energy
+                bound_to_star = star
+    return bound_to_star
+
+def identify_stellar_hosts(stars):
+    for star in stars:
+        star.bound_pebbles = Particles()
+    for star in stars:
+        for pebble in star.disk_particles:
+            bound_star = identify_host_star(pebble, stars)
+            pebble.star = bound_star
+            if not bound_star is None:
+                bound_star.bound_pebbles.add_particle(pebble)
+    for star in stars:
+        print "Bound pebbles:", star.mass, len(star.bound_pebbles)
+
+def identify_stellar_hosts_for_planets(stars):
+    for star in stars:
+        star.bound_planets = Particles()
+    for star in stars:
+        for planet in star.planets:
+            bound_star = identify_host_star(planet, stars)
+            planet.star = bound_star
+            if not bound_star is None:
+                bound_star.bound_planets.add_particle(planet)
+    for star in stars:
+        print "Bound planets:", star.mass, len(star.bound_planets)
+
+def remove_escapers(stars):
+    for star in stars:
+        pebbles = star.disk_particles
+        escapers = pebbles[pebbles.position.lengths()>2*stars.position.lengths().max()]
+        if len(escapers) == 0:
+            return
+
+        escapers = escapers[numpy.asarray([x is None for x in escapers.star])]
+        if len(escapers)>0:
+            pebbles.remove_particles(escapers)
+
+def remove_escaping_planets(stars):
+    for star in stars:
+        planets = star.planets
+        escapers = planets[planets.position.lengths()>2*stars.position.lengths().max()]
+        if len(escapers) == 0:
+            return
+
+        escapers = escapers[numpy.asarray([x is None for x in escapers.star])]
+        if len(escapers)>0:
+            planets.remove_particles(escapers)
+
+def new_option_parser():
+    result = OptionParser()
+    result.add_option("--seed", 
+                      dest="seed", type="int", default = 666,
+                      help="random number seed [%default]")
+    result.add_option("-N", 
+                      dest="Nstars", type="int", default = 3,
+                      help="Number of stars [%default]")
+    result.add_option("-f", 
+                      dest="filename", default = "initial_cluster.amuse",
+                      help="input filename")
+    result.add_option("-R", unit=units.parsec,
+                      dest="Rcluster", type="int", default = 1000|units.AU,
+                      help="Cluster size [%default]")
+    result.add_option("-n", 
+                      dest="n", type="int", default = 100,
+                      help="Number of pebbels per star [%default]")
+    return result
+
+
+if __name__ in ('__main__', '__plot__'):
+    o, arguments  = new_option_parser().parse_args()
+    #    random.seed(seed=o.seed)
+    if o.filename:
+        stars = read_set_from_file(o.filename, "amuse").copy()
+        Rcl = stars.position.lengths().max()
+        converter = nbody_system.nbody_to_si(stars.mass.sum(), Rcl)
+    else:
+        Ncl = o.Nstars
+        masses = new_salpeter_mass_distribution(Ncl)
+        Mcl = masses.sum()
+        Rcl = o.Rcluster
+        converter = nbody_system.nbody_to_si(Mcl, Rcl)
+        stars = new_plummer_model(Ncl, converter)
+        stars.mass = masses
+
+        arange = [10, 100] | units.AU
+        erange = [0., 0.1] 
+        mrange = [0.01, 10] | units.MJupiter
+        Nplanets = 10
+        Ndisk = o.n
+        for star in stars:
+            phi = numpy.radians(random.uniform(0, 90, 1)[0])#rotate under x
+            theta = numpy.radians(random.uniform(0, 180, 1)[0]) #rotate under y
+            star.disk_particles = make_pebble_disk(star, Ndisk, arange, erange, phi, theta)
+            star.planets = make_planets(star, Nplanets, arange, erange, mrange, phi, theta)
+            Mdisk = 1.0 | units.MJupiter
+            #        star.disk_particles.mass = Mdisk/float(Ndisk)
+            star.disk_particles.mass = Mdisk
+            star.bound_pebbles = Particles()
+
+    cluster = Hermite(converter)
+    cluster.parameters.dt_param = 0.0003
+    cluster.particles.add_particles(stars)
+    channel_from_cluster = cluster.particles.new_channel_to(stars)
+    tcr = Rcl/numpy.sqrt(constants.G*stars.mass.sum()/Rcl)
+    print "Tcr=", tcr.in_(units.yr)
+    dt = 10|units.yr
+    cluster_with_pebble_disks = bridge.Bridge(timestep=dt)
+
+    Johannes = Kepler(converter)
+    Johannes.initialize_code()
+
+    channels_from_planets = []
+    for star in stars:
+        cluster.particles.add_particles(star.planets)
+        channels_from_planets.append(
+            cluster.particles.new_channel_to(star.planets))
+
+    for star in stars:
+        pebble_gravity = advance_without_selfgravity(star.disk_particles)
+        stellar_gravity = central_point_mass(star)
+        cluster_with_pebble_disks.add_system(pebble_gravity, (cluster,))
+
+    cluster_with_pebble_disks.add_system(cluster, ())
+    identify_stellar_hosts(stars)
+    identify_stellar_hosts_for_planets(stars)
+    time = 0 | units.yr
+    identify_nearest_neighbor(stars, time)
+
+    if not o.filename:
+        filename = "initial_cluster.amuse"
+        write_set_to_file(stars, filename, "amuse", append_to_file=False, version="2.0")
+    
+    pyplot.ion()
+    istep = 0
+    while time < 100*tcr:
+        istep += 1
+        time += dt
+        cluster_with_pebble_disks.evolve_model(time)
+        channel_from_cluster.copy()
+        for channel in channels_from_planets:
+            channel.copy()
+        print time.in_(units.yr), cluster_with_pebble_disks.model_time.in_(units.yr)
+        movie(stars)
+        if istep%10 == 0:
+            remove_escapers(stars)
+            remove_escaping_planets(stars)
+            identify_stellar_hosts(stars)
+            identify_stellar_hosts_for_planets(stars)
+            determine_orbital_parameters_of_planets(stars)
+            determine_orbital_parameters_of_pebbles(stars)
+            identify_nearest_neighbor(stars, time)
+
+#            filename = "cluster{0}.amuse".format(istep)
+#            write_set_to_file(stars, filename, "amuse")
+    pyplot.show()
+    Johannes.stop()
---
+++ spz/PebbleDisk/evolve_disk.py
@@ -0,0 +1,100 @@
+import sys
+import numpy
+from numpy import random
+from amuse.lab import *
+from amuse.couple import bridge
+from make_pebble_disk import make_pebble_disk
+from amuse.units.optparse import OptionParser
+from matplotlib import pyplot
+
+def movie(particles):
+    pyplot.scatter(disk_particles.x.value_in(units.AU), disk_particles.y.value_in(units.AU))
+    pyplot.draw()
+    pyplot.cla()
+
+class central_point_mass(object):
+    def __init__(self, particle):
+        self.particle = particle
+
+    def get_gravity_at_point(self,eps,x,y,z):
+        dx = x-self.particle.x
+        dy = y-self.particle.y
+        dz = z-self.particle.z
+
+        r2=dx**2 + dy**2 + dz**2
+        r=r2**0.5
+        fr=constants.G*self.particle.mass/r2
+        ax=-fr*dx/r
+        ay=-fr*dy/r
+        az=-fr*dz/r
+        return ax,ay,az
+
+    def get_potential_at_point(self,eps,x,y,z):
+        r2=(x-self.particle.x)**2+(y-self.particle.y)**2+(z-self.particle.z)**2
+        return constant.G*self.particle.mass/r2
+
+class advance_without_selfgravity(object):
+    """
+    to advance particles
+    """
+    def __init__(self, particles, time= 0 |units.yr):
+        self._particles = particles
+        self.model_time = time
+
+    @property
+    def particles(self):
+        return self._particles
+
+    def evolve_model(self, t_end):
+        dt = t_end - self.model_time
+        self._particles.position += self._particles.velocity*dt
+        self.model_time= t_end
+    
+    @property
+    def potential_energy(self):
+        return quantities.zero
+    
+    @property 
+    def kinetic_energy(self):
+        return (0.5*self.particles.mass*self.particles.velocity.lengths()**2).sum()
+
+def new_option_parser():
+    result = OptionParser()
+    result.add_option("--seed", 
+                      dest="seed", type="int", default = 666,
+                      help="random number seed [%default]")
+    result.add_option("-n", 
+                      dest="n", type="int", default = 1000,
+                      help="Number of stars [%default]")
+    return result
+
+if __name__ in ('__main__', '__plot__'):
+    o, arguments  = new_option_parser().parse_args()
+    random.seed(seed=o.seed)
+
+    arange = [10, 100] | units.AU
+    erange = [0., 0.1] 
+    Ndisk = o.n
+    Mstar = 10|units.MSun
+    central_particle = Particle(mass=Mstar, position=(100,0,0)|units.AU, velocity=(0,0,0)|units.kms)
+
+    disk_particles = make_pebble_disk(central_particle, Ndisk, arange, erange)
+    disk_particles.mass = 0 | units.MSun
+
+    converter = nbody_system.nbody_to_si(Mstar, arange[-1])
+
+    pebble_gravity = advance_without_selfgravity(disk_particles)
+    stellar_gravity = central_point_mass(central_particle)
+
+    dt = 0.1 | units.yr
+    gravity = bridge.Bridge(timestep=0.1*dt)
+    gravity.add_system(pebble_gravity, (stellar_gravity,), False)
+    
+    time = zero
+    pyplot.ion()
+    while time < 100|units.yr:
+        time += dt
+        gravity.evolve_model(time)
+        print time, gravity.model_time
+        movie(pebble_gravity.particles)
+    pyplot.show()
